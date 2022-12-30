9664dec53c7587ff2baa71f0ac228f5b89a7730d
manh quan <luumanhquan.91@gmail.com>
2022-12-30 21:55:19 +0700

    .

--- upload_service/pkg/id/id.go
+++ upload_service/pkg/id/id.go
@@ -1,1 +1,21 @@
 package id
+
+import (
+	"github.com/google/uuid"
+)
+
+type uuidData struct{}
+
+type UUIDData interface {
+	GearedID() string
+}
+
+func NewSnowflake() UUIDData {
+	return &uuidData{}
+}
+
+func (u *uuidData) GearedID() string {
+	id := uuid.New()
+
+	return id.String()
+}
