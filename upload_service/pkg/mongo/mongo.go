fb7075f34e8fc1e1bd1da0f586f1fd4b5158d27f
manh quan <luumanhquan.91@gmail.com>
2022-12-30 21:49:21 +0700

    .

--- upload_service/pkg/mongo/mongo.go
+++ upload_service/pkg/mongo/mongo.go
@@ -14,7 +14,7 @@ type MongoDB struct {
 }
 
 // mongodb://localhost:27017
-func Connect(dns string) (*MongoDB, error) {
+func ConnectMongoDB(dns string) (*MongoDB, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(dns))
