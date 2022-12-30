5d1e67fcd54d9f951bdbf9b0a06d83160dc96591
manh quan <luumanhquan.91@gmail.com>
2022-12-30 22:20:13 +0700

    add model

--- upload_service/model/media.go
+++ upload_service/model/media.go
@@ -1,5 +1,24 @@
 package model
 
+import "time"
+
 type Media struct {
-	Id string
+	Id         string
+	FileName   string
+	URL        string
+	CreateDate time.Time
+	UpdateDate time.Time
+}
+
+type UpdateMedia struct {
+	FileName   string
+	URL        string
+	UpdateDate time.Time
+}
+
+type CreateMedia struct {
+	Id         string
+	FileName   string
+	URL        string
+	CreateDate time.Time
 }
