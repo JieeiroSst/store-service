d1c5620af7dd4b55642220d2cf6fc37dfc06b519
manh quan <luumanhquan.91@gmail.com>
2022-12-31 14:14:20 +0700

    add ci/cd

--- upload_service/internal/delivery/http/http.go
+++ upload_service/internal/delivery/http/http.go
@@ -1,19 +1,30 @@
 package http
 
-import "github.com/gofiber/fiber/v2"
+import (
+	v1 "github.com/JIeeiroSst/upload-service/internal/delivery/http/v1"
+	"github.com/JIeeiroSst/upload-service/internal/usecase"
+	"github.com/gofiber/fiber/v2"
+)
 
 type Handler struct {
+	usecase usecase.Usecase
 }
 
-func NewHandler() *Handler {
-	return &Handler{}
+func NewHandler(usecase usecase.Usecase) *Handler {
+	return &Handler{
+		usecase: usecase,
+	}
 }
 
-func (h *Handler) Init(router fiber.App) {
+func (h *Handler) Init(router *fiber.App) {
 	h.corsMiddleware(router)
 	h.initApi(router)
 }
 
-func (h *Handler) initApi(router fiber.App) {
-
+func (h *Handler) initApi(router *fiber.App) {
+	handlerV1 := v1.NewHandler(h.usecase)
+	api := router.Group("/api")
+	{
+		handlerV1.Init(api)
+	}
 }
