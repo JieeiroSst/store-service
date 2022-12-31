b1db8dcb133fc86cc2bc24a49ccb9bfc43859ee8
manh quan <luumanhquan.91@gmail.com>
2022-12-31 13:45:55 +0700

    update call api upload file image

--- upload_service/internal/repository/upload.go
+++ upload_service/internal/repository/upload.go
@@ -11,7 +11,7 @@ import (
 
 type Uploads interface {
 	Create(ctx context.Context, upload model.CreateMedia) error
-	Update(ctx context.Context, upload model.UpdateMedia) error
+	Update(ctx context.Context, id string, upload model.UpdateMedia) error
 	GetAll(ctx context.Context) ([]model.Media, error)
 	GetById(ctx context.Context, id string) (*model.Media, error)
 	Delete(ctx context.Context, id string) error
@@ -28,22 +28,48 @@ func NewUploadRepo(Collection *mongo.Collection) *UploadRepo {
 }
 
 func (r *UploadRepo) Create(ctx context.Context, upload model.CreateMedia) error {
+	_, err := r.collection.InsertOne(ctx, upload)
+	if err != nil {
+		log.Error(err.Error())
+		return err
+	}
 
 	return nil
 }
 
-func (r *UploadRepo) Update(ctx context.Context, upload model.UpdateMedia) error {
-
+func (r *UploadRepo) Update(ctx context.Context, id string, upload model.UpdateMedia) error {
+	filter := bson.D{{Key: "_id", Value: id}}
+	_, err := r.collection.UpdateOne(context.TODO(), filter, upload)
+	if err != nil {
+		log.Error(err.Error())
+		return err
+	}
 	return nil
 }
 
 func (r *UploadRepo) GetAll(ctx context.Context) ([]model.Media, error) {
-	return nil, nil
+	var result []model.Media
+	cur, err := r.collection.Find(ctx, bson.D{})
+	if err != nil {
+		return nil, err
+	}
+	defer cur.Close(ctx)
+	for cur.Next(ctx) {
+		if err := cur.Decode(&result); err != nil {
+			log.Error(err.Error())
+			return nil, err
+		}
+	}
+	if err := cur.Err(); err != nil {
+		log.Error(err.Error())
+		return nil, err
+	}
+	return result, nil
 }
 
 func (r *UploadRepo) GetById(ctx context.Context, id string) (*model.Media, error) {
 	var result *model.Media
-	filter := bson.D{{Key: "id", Value: id}}
+	filter := bson.D{{Key: "_id", Value: id}}
 	err := r.collection.FindOne(ctx, filter).Decode(&result)
 	if err != nil {
 		log.Error(err.Error())
@@ -54,5 +80,11 @@ func (r *UploadRepo) GetById(ctx context.Context, id string) (*model.Media, erro
 }
 
 func (r *UploadRepo) Delete(ctx context.Context, id string) error {
+	filter := bson.D{{Key: "_id", Value: id}}
+	_, err := r.collection.DeleteOne(ctx, filter)
+	if err != nil {
+		log.Error(err.Error())
+		return err
+	}
 	return nil
 }
--- upload_service/model/media.go
+++ upload_service/model/media.go
@@ -3,22 +3,22 @@ package model
 import "time"
 
 type Media struct {
-	Id         string
-	FileName   string
-	URL        string
-	CreateDate time.Time
-	UpdateDate time.Time
+	Id         string    `json:"id" bson:"_id,omitempty"`
+	FileName   string    `json:"file_name" bson:"file_name,omitempty"`
+	URL        string    `json:"url" bson:"url,omitempty"`
+	CreateDate time.Time `json:"create_date" bson:"create_date,omitempty"`
+	UpdateDate time.Time `json:"update_date" bson:"update_date"`
 }
 
 type UpdateMedia struct {
-	FileName   string
-	URL        string
-	UpdateDate time.Time
+	FileName   string    `json:"file_name" bson:"file_name,omitempty"`
+	URL        string    `json:"url" bson:"url,omitempty"`
+	UpdateDate time.Time `json:"update_date" bson:"update_date,omitempty"`
 }
 
 type CreateMedia struct {
-	Id         string
-	FileName   string
-	URL        string
-	CreateDate time.Time
+	Id         string    `json:"id" bson:"_id,omitempty"`
+	FileName   string    `json:"file_name" bson:"file_name,omitempty"`
+	URL        string    `json:"url" bson:"url,omitempty"`
+	CreateDate time.Time `json:"create_date" bson:"create_date,omitempty"`
 }
