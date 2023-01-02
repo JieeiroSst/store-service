dd00af3566c47f3a2b82e945a239cfe5ca2b498e
manh quan <luumanhquan.91@gmail.com>
2023-01-02 15:49:54 +0700

    add code upload file

--- upload_service/internal/delivery/http/v1/upload.go
+++ upload_service/internal/delivery/http/v1/upload.go
@@ -1,12 +1,42 @@
 package v1
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func (h *Handler) initUploadRoutes(api fiber.Router) {
 
 }
 
-func (h *Handler) UploadFile() error {
+func (h *Handler) UploadFile(ctx *fiber.Ctx) error {
+	file, err := ctx.FormFile("image")
+	if err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": false,
+			"msg":   err.Error(),
+		})
+	}
 
-	return nil
+	fileMutipart, err := file.Open()
+	if err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": false,
+			"msg":   err.Error(),
+		})
+	}
+
+	receiverId := ctx.Query("receiver_id")
+
+	err = h.usecase.Create(ctx.Context(), fileMutipart, file, receiverId)
+	if err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": false,
+			"msg":   err.Error(),
+		})
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+		"error": true,
+		"msg":   "FILE UPLOAD SUCCESS MESSAGE",
+	})
 }
