1abddfdc546bccb45018d9e23821a4be2e91816b
manh quan <luumanhquan.91@gmail.com>
2023-03-12 00:26:09 +0700

    .

--- authorize_service/internal/delivery/http/v1/socket.go
+++ authorize_service/internal/delivery/http/v1/socket.go
@@ -1,18 +1,19 @@
 package v1
 
 import (
-	"flag"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
 )
 
-var addr = flag.String("addr", "localhost:8080", "http service address")
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
-var upgrader = websocket.Upgrader{} // use default options
-
-func echo(w http.ResponseWriter, r *http.Request) {
+func Echo(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Print("upgrade:", err)
