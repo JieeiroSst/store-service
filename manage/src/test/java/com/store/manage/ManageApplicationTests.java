ed0d21fe54222f8b2ee2006f51025cfe9d838e44
manh quan <luumanhquan.91@gmail.com>
2023-04-15 16:45:06 +0700

    update code

---
+++ manage/src/test/java/com/store/manage/ManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
