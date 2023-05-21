03705a7b773819b6405b0b1bcd487606c102709f
manh quan <luumanhquan.91@gmail.com>
2023-05-21 14:48:51 +0700

    update code

--- manage_service/internal/repository/repository.go
+++ manage_service/internal/repository/repository.go
@@ -7,12 +7,10 @@ import (
 
 type Repositories struct {
 	UserKeycloak
-	UserDB
 }
 
 func NewRepositories(db *gorm.DB, client keycloak.GoCloak) *Repositories {
 	return &Repositories{
 		UserKeycloak: NewUserKeycloakRepo(client),
-		UserDB:       NewUserDBRepo(db),
 	}
 }
