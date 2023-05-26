22494e945772b1050c39eb7abda114862c609550
manh quan <luumanhquan.91@gmail.com>
2023-05-27 00:48:37 +0700

    update code

--- chat_service/internal/delivery/websocket/websocket.go
+++ chat_service/internal/delivery/websocket/websocket.go
@@ -1,20 +1,18 @@
-package main
- 
+package websocket
+
 import (
-	"fmt"
 	"log"
 	"net/http"
- 
+
 	"github.com/gorilla/websocket"
 )
- 
+
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 	CheckOrigin:     func(r *http.Request) bool { return true },
 }
- 
- 
+
 func reader(conn *websocket.Conn) {
 	for {
 		messageType, p, err := conn.ReadMessage()
@@ -22,42 +20,27 @@ func reader(conn *websocket.Conn) {
 			log.Println(err)
 			return
 		}
-		log.Println("========>",string(p))
- 
+		log.Println("========>", string(p))
+
 		if err := conn.WriteMessage(messageType, p); err != nil {
 			log.Println(err)
 			return
 		}
- 
+
 	}
 }
- 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Home Page")
-}
- 
-func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+
+func WebSocketEndpoint(w http.ResponseWriter, r *http.Request) {
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 	}
- 
+
 	log.Println("Client Connected")
 	err = ws.WriteMessage(1, []byte("Hi Client!"))
 	if err != nil {
 		log.Println(err)
 	}
- 
+
 	reader(ws)
-}
- 
-func setupRoutes() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/ws", wsEndpoint)
-}
- 
-func main() {
-	fmt.Println("Hello World")
-	setupRoutes()
-	log.Fatal(http.ListenAndServe(":6060", nil))
 }
