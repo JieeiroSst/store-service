01125611704e9cabd83f7807b5006d38027c0b62
manh quan <luumanhquan.91@gmail.com>
2023-05-26 19:23:54 +0700

    update code

---
+++ chat_service/pkg/mongo/mongo.go
@@ -0,0 +1,25 @@
+package mongo
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Mongo struct {
+	client *mongo.Client
+}
+
+func NewMongo(host string) *Mongo {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	dns := fmt.Sprintf("mongodb://%v", host)
+	clientOptions := options.Client().ApplyURI(dns)
+	client, _ := mongo.Connect(ctx, clientOptions)
+	return &Mongo{
+		client: client,
+	}
+}
---
+++ chat_service/pkg/snowflake/snowflake.go
@@ -0,0 +1,29 @@
+package snowflake
+
+import (
+	"strconv"
+
+	"github.com/bwmarrin/snowflake"
+)
+
+type snowflakeData struct{}
+
+type SnowflakeData interface {
+	GearedID() int
+}
+
+func NewSnowflake() SnowflakeData {
+	return &snowflakeData{}
+}
+
+func (s *snowflakeData) GearedID() int {
+	n, err := snowflake.NewNode(1)
+	if err != nil {
+		return 0
+	}
+	id, err := strconv.Atoi(n.Generate().String())
+	if err != nil {
+		return 0
+	}
+	return id
+}
