0f0c656bd7da384961e60140ff03c7927b2492a7
manh quan <luumanhquan.91@gmail.com>
2023-05-27 21:10:26 +0700

    update code

--- chat_service/internal/usecase/message.go
+++ chat_service/internal/usecase/message.go
@@ -1,11 +1,22 @@
 package usecase
 
+import (
+	"github.com/JIeeiroSst/chat-service/internal/repository"
+	"github.com/JIeeiroSst/chat-service/pkg/cache"
+)
+
 type Messages interface {
 }
 
 type Messagesecase struct {
+	MessageRepo repository.MessageRepo
+	CacheHelper cache.CacheHelper
 }
 
-func NewOTPUsecase() *Messagesecase {
-	return &Messagesecase{}
+func NewMessageUsecase(MessageRepo repository.MessageRepo, CacheHelper cache.CacheHelper) *Messagesecase {
+	return &Messagesecase{
+		MessageRepo: MessageRepo,
+		CacheHelper: CacheHelper,
+	}
 }
+
--- chat_service/internal/usecase/usecase.go
+++ chat_service/internal/usecase/usecase.go
@@ -1,14 +1,21 @@
 package usecase
 
-import "github.com/JIeeiroSst/chat-service/pkg/cache"
+import (
+	"github.com/JIeeiroSst/chat-service/internal/repository"
+	"github.com/JIeeiroSst/chat-service/pkg/cache"
+)
 
 type Usecase struct {
+	Messages
 }
 
 type Dependency struct {
 	CacheHelper cache.CacheHelper
+	MessageRepo repository.MessageRepo
 }
 
 func NewUsecase(deps Dependency) *Usecase {
-	return &Usecase{}
+	return &Usecase{
+		Messages: NewMessageUsecase(deps.MessageRepo, deps.CacheHelper),
+	}
 }
