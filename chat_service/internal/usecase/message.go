59d57807dc582e5ef90a6941c94f147868d17361
manh quan <luumanhquan.91@gmail.com>
2023-05-27 21:15:09 +0700

    update code

--- chat_service/internal/usecase/message.go
+++ chat_service/internal/usecase/message.go
@@ -1,22 +1,53 @@
 package usecase
 
 import (
+	"context"
+
+	"github.com/JIeeiroSst/chat-service/dto"
 	"github.com/JIeeiroSst/chat-service/internal/repository"
 	"github.com/JIeeiroSst/chat-service/pkg/cache"
+	"github.com/JIeeiroSst/chat-service/pkg/snowflake"
 )
 
 type Messages interface {
+	SaveMessage(ctx context.Context, message dto.Messages) error
+	GetMessageById(ctx context.Context, id int) (*dto.Messages, error)
+	CreateReport(ctx context.Context, report dto.Reports) error
+	GetReportByUser(ctx context.Context, userId int) ([]dto.Reports, error)
+	DeleteMessage(ctx context.Context, messageId, userId int) error
 }
 
 type Messagesecase struct {
 	MessageRepo repository.MessageRepo
 	CacheHelper cache.CacheHelper
+	Snowflake   snowflake.SnowflakeData
 }
 
-func NewMessageUsecase(MessageRepo repository.MessageRepo, CacheHelper cache.CacheHelper) *Messagesecase {
+func NewMessageUsecase(MessageRepo repository.MessageRepo, cacheHelper cache.CacheHelper,
+	snowflake snowflake.SnowflakeData) *Messagesecase {
 	return &Messagesecase{
 		MessageRepo: MessageRepo,
-		CacheHelper: CacheHelper,
+		CacheHelper: cacheHelper,
+		Snowflake:   snowflake,
 	}
 }
 
+func (u *Messagesecase) SaveMessage(ctx context.Context, message dto.Messages) error {
+	return nil
+}
+
+func (u *Messagesecase) GetMessageById(ctx context.Context, id int) (*dto.Messages, error) {
+	return nil, nil
+}
+
+func (u *Messagesecase) CreateReport(ctx context.Context, report dto.Reports) error {
+	return nil
+}
+
+func (u *Messagesecase) GetReportByUser(ctx context.Context, userId int) ([]dto.Reports, error) {
+	return nil, nil
+}
+
+func (u *Messagesecase) DeleteMessage(ctx context.Context, messageId, userId int) error {
+	return nil
+}
--- chat_service/internal/usecase/usecase.go
+++ chat_service/internal/usecase/usecase.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"github.com/JIeeiroSst/chat-service/internal/repository"
 	"github.com/JIeeiroSst/chat-service/pkg/cache"
+	"github.com/JIeeiroSst/chat-service/pkg/snowflake"
 )
 
 type Usecase struct {
@@ -12,10 +13,11 @@ type Usecase struct {
 type Dependency struct {
 	CacheHelper cache.CacheHelper
 	MessageRepo repository.MessageRepo
+	Snowflake   snowflake.SnowflakeData
 }
 
 func NewUsecase(deps Dependency) *Usecase {
 	return &Usecase{
-		Messages: NewMessageUsecase(deps.MessageRepo, deps.CacheHelper),
+		Messages: NewMessageUsecase(deps.MessageRepo, deps.CacheHelper, deps.Snowflake),
 	}
 }
