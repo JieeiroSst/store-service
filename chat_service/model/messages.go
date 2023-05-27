747e894ad95f46c1d39e29b16caa26683bc6ef49
manh quan <luumanhquan.91@gmail.com>
2023-05-27 20:32:27 +0700

    update code

--- chat_service/model/messages.go
+++ chat_service/model/messages.go
@@ -1,21 +1,54 @@
 package model
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
-type MessageType string
+type MESSAGETYPE string
 
 const (
-	TEXT  MessageType = "text"
-	IMAGE MessageType = "image"
-	VIDEO MessageType = "video"
-	AUDIO MessageType = "audio"
+	TEXT  MESSAGETYPE = "text"
+	IMAGE MESSAGETYPE = "image"
+	VIDEO MESSAGETYPE = "video"
+	AUDIO MESSAGETYPE = "audio"
 )
 
 type Messages struct {
 	ID          int         `json:"_id,omitempty" bson:"_id,omitempty"`
 	Guid        string      `json:"guid,omitempty" bson:"guid,omitempty"`
 	SenderId    int         `json:"sender_id,omitempty" bson:"sender_id,omitempty"`
-	MessageType MessageType `json:"message_type,omitempty" bson:"message_type,omitempty"`
+	MessageType MESSAGETYPE `json:"message_type,omitempty" bson:"message_type,omitempty"`
 	CreatedAt   time.Time   `json:"created_at,omitempty" bson:"created_at,omitempty"`
 	DeletedAt   time.Time   `json:"deleted_at,omitempty" bson:"deleted_at,omitempty"`
+}
+
+func (m MESSAGETYPE) String() string {
+	switch m {
+	case TEXT:
+		return "text"
+	case IMAGE:
+		return "image"
+	case VIDEO:
+		return "video"
+	case AUDIO:
+		return "audio"
+	default:
+		return fmt.Sprintf("%v", string(m))
+	}
+}
+
+func ParseMessageType(s string) (c MESSAGETYPE, err error) {
+	status := map[MESSAGETYPE]struct{}{
+		TEXT:  {},
+		IMAGE: {},
+		VIDEO: {},
+		AUDIO: {},
+	}
+	cap := MESSAGETYPE(s)
+	_, ok := status[cap]
+	if !ok {
+		return c, fmt.Errorf(`cannot parse:[%s] as capability`, s)
+	}
+	return cap, nil
 }
--- chat_service/model/reports.go
+++ chat_service/model/reports.go
@@ -1,8 +1,12 @@
 package model
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type STATUS string
+
 const (
 	PENDING  STATUS = "pending"
 	RESOLVED STATUS = "resolved"
@@ -16,4 +20,28 @@ type Reports struct {
 	Notes          string    `json:"notes,omitempty" bson:"notes,omitempty"`
 	Status         STATUS    `json:"status,omitempty" bson:"status,omitempty"`
 	CreatedAt      time.Time `json:"created_at,omitempty" bson:"created_at,omitempty"`
+}
+
+func (e STATUS) String() string {
+	switch e {
+	case PENDING:
+		return "pending"
+	case RESOLVED:
+		return "resolved"
+	default:
+		return fmt.Sprintf("%v", string(e))
+	}
+}
+
+func ParseStatus(s string) (c STATUS, err error) {
+	status := map[STATUS]struct{}{
+		PENDING:  {},
+		RESOLVED: {},
+	}
+	cap := STATUS(s)
+	_, ok := status[cap]
+	if !ok {
+		return c, fmt.Errorf(`cannot parse:[%s] as capability`, s)
+	}
+	return cap, nil
 }
