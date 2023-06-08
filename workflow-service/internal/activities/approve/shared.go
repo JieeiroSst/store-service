77deb51c194118b42508cba30f39ef245b3f2d57
manh quan <luumanhquan.91@gmail.com>
2023-06-08 19:59:42 +0700

    update code

---
+++ workflow-service/internal/activities/approve/shared.go
@@ -0,0 +1,25 @@
+package approve
+
+var SignalChannels = struct {
+	UPLOAD_CHANNEL  string
+	PROCESS_CHANNEL string
+	APPROVE_CHANNEL string
+}{
+	UPLOAD_CHANNEL:  "UPLOAD_CHANNEL",
+	PROCESS_CHANNEL: "PROCESS_CHANNEL",
+	APPROVE_CHANNEL: "APPROVE_CHANNEL",
+}
+
+var RouteTypes = struct {
+	UPLOAD  string
+	PROCESS string
+	APPROVE string
+}{
+	UPLOAD:  "UPLOAD",
+	PROCESS: "PROCESS",
+	APPROVE: "APPROVE",
+}
+
+type RouteSignal struct {
+	Route string
+}
