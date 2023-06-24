00df48645747d34d2e17554b7850d620c2dfdc7c
manh quan <luumanhquan.91@gmail.com>
2023-06-24 15:57:29 +0700

    update code

--- workflow-service/internal/activities/approve/facade/seattle-weather.go
+++ workflow-service/internal/activities/approve/facade/seattle-weather.go
@@ -1,15 +1,19 @@
 package facade
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/JIeeiroSst/workflow-service/dto"
 	"github.com/JIeeiroSst/workflow-service/internal/repository"
+	"github.com/JIeeiroSst/workflow-service/model"
 )
 
 type SeattleWeather interface {
-	upsertBigQuerySeattleWeather(weathers []dto.SeattleWeatherRequestDTO)
-	processSeattleWeather(weathers <-chan dto.SeattleWeatherRequestDTO, batchSize int)
-	produceSeattleWeather(weathers []dto.SeattleWeatherRequestDTO, to chan dto.SeattleWeatherRequestDTO)
-	InsertSeattleWeather(weathers []dto.SeattleWeatherRequestDTO)
+	upsertBigQuerySeattleWeather(seattleWeathers []dto.SeattleWeatherRequestDTO)
+	processSeattleWeather(seattleWeathers <-chan dto.SeattleWeatherRequestDTO, batchSize int)
+	produceSeattleWeather(seattleWeathers []dto.SeattleWeatherRequestDTO, to chan dto.SeattleWeatherRequestDTO)
+	InsertSeattleWeather(seattleWeathers []dto.SeattleWeatherRequestDTO)
 }
 
 type SeattleWeatherFacade struct {
@@ -22,14 +26,66 @@ func NewSeattleWeatherFacade(repository *repository.Repositories) *SeattleWeathe
 	}
 }
 
-func (u *SeattleWeatherFacade) upsertBigQuerySeattleWeather(weathers []dto.SeattleWeatherRequestDTO) {
-
+func (u *SeattleWeatherFacade) upsertBigQuerySeattleWeather(seattleWeathers []dto.SeattleWeatherRequestDTO) {
+	fmt.Printf("Processing batch of %d\n", len(seattleWeathers))
+	var seattleWeathersModels []model.SeattleWeather
+	for _, game := range seattleWeathers {
+		seattleWeathersModel := u.mappingSeattleWeather(game)
+		seattleWeathersModels = append(seattleWeathersModels, seattleWeathersModel)
+	}
+	if err := u.repository.SeattleWeathers.InsertBatchSeattleWeathers(seattleWeathersModels); err != nil {
+		log.Println(err)
+	}
 }
 
-func (u *SeattleWeatherFacade) processSeattleWeather(weathers <-chan dto.SeattleWeatherRequestDTO, batchSize int) {
+func (u *SeattleWeatherFacade) processSeattleWeather(seattleWeathers <-chan dto.SeattleWeatherRequestDTO, batchSize int) {
+	var batch []dto.SeattleWeatherRequestDTO
+	for seattleWeather := range seattleWeathers {
+		batch = append(batch, seattleWeather)
+		if len(batch) == batchSize {
+			u.upsertBigQuerySeattleWeather(batch)
+			batch = []dto.SeattleWeatherRequestDTO{}
+		}
+	}
+	if len(batch) > 0 {
+		u.upsertBigQuerySeattleWeather(batch)
+	}
 }
 
-func (u *SeattleWeatherFacade) produceSeattleWeather(weathers []dto.SeattleWeatherRequestDTO, to chan dto.SeattleWeatherRequestDTO) {
+func (u *SeattleWeatherFacade) produceSeattleWeather(seattleWeathers []dto.SeattleWeatherRequestDTO, to chan dto.SeattleWeatherRequestDTO) {
+	for _, seattleWeather := range seattleWeathers {
+		to <- dto.SeattleWeatherRequestDTO{
+			Date:          seattleWeather.Date,
+			Precipitation: seattleWeather.Precipitation,
+			TempMax:       seattleWeather.TempMax,
+			TempMin:       seattleWeather.TempMin,
+			Wind:          seattleWeather.Wind,
+			Weather:       seattleWeather.Weather,
+		}
+	}
 }
 
-func (u *SeattleWeatherFacade) InsertSeattleWeather(weathers []dto.SeattleWeatherRequestDTO) {}
+func (u *SeattleWeatherFacade) InsertSeattleWeather(seattleWeathers []dto.SeattleWeatherRequestDTO) {
+	var batch []dto.SeattleWeatherRequestDTO
+	for _, seattleWeather := range seattleWeathers {
+		batch = append(batch, seattleWeather)
+		if len(batch) == batchSize {
+			u.upsertBigQuerySeattleWeather(batch)
+			batch = []dto.SeattleWeatherRequestDTO{}
+		}
+	}
+	if len(batch) > 0 {
+		u.upsertBigQuerySeattleWeather(batch)
+	}
+}
+
+func (u *SeattleWeatherFacade) mappingSeattleWeather(seattleWeather dto.SeattleWeatherRequestDTO) model.SeattleWeather {
+	return model.SeattleWeather{
+		Date:          seattleWeather.Date,
+		Precipitation: seattleWeather.Precipitation,
+		TempMax:       seattleWeather.TempMax,
+		TempMin:       seattleWeather.TempMin,
+		Wind:          seattleWeather.Wind,
+		Weather:       seattleWeather.Weather,
+	}
+}
--- workflow-service/internal/activities/approve/facade/spotify-quarterly.go
+++ workflow-service/internal/activities/approve/facade/spotify-quarterly.go
@@ -1,15 +1,20 @@
 package facade
 
 import (
+	"fmt"
+	"log"
+	"sync"
+
 	"github.com/JIeeiroSst/workflow-service/dto"
 	"github.com/JIeeiroSst/workflow-service/internal/repository"
+	"github.com/JIeeiroSst/workflow-service/model"
 )
 
 type SpotifyQuarterly interface {
-	upsertBigQuerySpotifyQuarterly(weathers []dto.SpotifyQuarterlyRequestDTO)
-	processSpotifyQuarterly(weathers <-chan dto.SpotifyQuarterlyRequestDTO, batchSize int)
-	produceSpotifyQuarterly(weathers []dto.SpotifyQuarterlyRequestDTO, to chan dto.SpotifyQuarterlyRequestDTO)
-	InsertSpotifyQuarterly(weathers []dto.SpotifyQuarterlyRequestDTO)
+	upsertBigQuerySpotifyQuarterly(spotifyQuarterlies []dto.SpotifyQuarterlyRequestDTO)
+	processSpotifyQuarterly(spotifyQuarterlies <-chan dto.SpotifyQuarterlyRequestDTO, batchSize int)
+	produceSpotifyQuarterly(spotifyQuarterlies []dto.SpotifyQuarterlyRequestDTO, to chan dto.SpotifyQuarterlyRequestDTO)
+	InsertSpotifyQuarterly(spotifyQuarterlies []dto.SpotifyQuarterlyRequestDTO)
 }
 
 type SpotifyQuarterlyFacade struct {
@@ -22,15 +27,90 @@ func NewSpotifyQuarterlyFacade(repository *repository.Repositories) *SellingPlay
 	}
 }
 
-func (u *SellingPlayStationFacade) upsertBigQuerySpotifyQuarterly(weathers []dto.SpotifyQuarterlyRequestDTO) {
-
+func (u *SellingPlayStationFacade) upsertBigQuerySpotifyQuarterly(spotifyQuarterlies []dto.SpotifyQuarterlyRequestDTO) {
+	fmt.Printf("Processing batch of %d\n", len(spotifyQuarterlies))
+	var spotifyQuarterlyModels []model.SpotifyQuarterly
+	for _, spotifyQuarterly := range spotifyQuarterlies {
+		spotifyQuarterlyModel := u.mappingSpotifyQuarterly(spotifyQuarterly)
+		spotifyQuarterlyModels = append(spotifyQuarterlyModels, spotifyQuarterlyModel)
+	}
+	if err := u.repository.SpotifyQuarterlys.InsertBatchSpotifyQuarterlys(spotifyQuarterlyModels); err != nil {
+		log.Println(err)
+	}
 }
 
-func (u *SellingPlayStationFacade) processSpotifyQuarterly(weathers <-chan dto.SpotifyQuarterlyRequestDTO, batchSize int) {
+func (u *SellingPlayStationFacade) processSpotifyQuarterly(spotifyQuarterlies <-chan dto.SpotifyQuarterlyRequestDTO, batchSize int) {
+	var batch []dto.SpotifyQuarterlyRequestDTO
+	for spotifyQuarterlie := range spotifyQuarterlies {
+		batch = append(batch, spotifyQuarterlie)
+		if len(batch) == batchSize {
+			u.upsertBigQuerySpotifyQuarterly(batch)
+			batch = []dto.SpotifyQuarterlyRequestDTO{}
+		}
+	}
+	if len(batch) > 0 {
+		u.upsertBigQuerySpotifyQuarterly(batch)
+	}
 }
 
-func (u *SellingPlayStationFacade) produceSpotifyQuarterly(weathers []dto.SpotifyQuarterlyRequestDTO, to chan dto.SpotifyQuarterlyRequestDTO) {
+func (u *SellingPlayStationFacade) produceSpotifyQuarterly(spotifyQuarterlies []dto.SpotifyQuarterlyRequestDTO, to chan dto.SpotifyQuarterlyRequestDTO) {
+	for _, spotifyQuarterly := range spotifyQuarterlies {
+		to <- dto.SpotifyQuarterlyRequestDTO{
+			Date:                       spotifyQuarterly.Date,
+			TotalRevenue:               spotifyQuarterly.TotalRevenue,
+			CostOfRevenue:              spotifyQuarterly.CostOfRevenue,
+			GrossProfit:                spotifyQuarterly.GrossProfit,
+			PremiumRevenue:             spotifyQuarterly.PremiumRevenue,
+			PremiumCostRevenue:         spotifyQuarterly.PremiumCostRevenue,
+			PremiumGrossProfit:         spotifyQuarterly.PremiumGrossProfit,
+			AdRevenue:                  spotifyQuarterly.AdRevenue,
+			AdCostOfRevenue:            spotifyQuarterly.AdCostOfRevenue,
+			AdGrossProfit:              spotifyQuarterly.AdGrossProfit,
+			MAUs:                       spotifyQuarterly.MAUs,
+			PremiumARPU:                spotifyQuarterly.PremiumARPU,
+			AdMAUs:                     spotifyQuarterly.AdMAUs,
+			SalesAndMarketingCost:      spotifyQuarterly.SalesAndMarketingCost,
+			ResearchAndDevelopmentCost: spotifyQuarterly.ResearchAndDevelopmentCost,
+			GenrealAndAdminstraiveCost: spotifyQuarterly.GenrealAndAdminstraiveCost,
+		}
+	}
 }
 
-func (u *SellingPlayStationFacade) InsertSpotifyQuarterly(weathers []dto.SpotifyQuarterlyRequestDTO) {
+func (u *SellingPlayStationFacade) InsertSpotifyQuarterly(spotifyQuarterlies []dto.SpotifyQuarterlyRequestDTO) {
+	var wg sync.WaitGroup
+	audits := make(chan dto.SpotifyQuarterlyRequestDTO)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		u.processSpotifyQuarterly(audits, batchSize)
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		u.produceSpotifyQuarterly(spotifyQuarterlies, audits)
+		close(audits)
+	}()
+	wg.Wait()
+	fmt.Println("Complete")
+}
+
+func (u *SellingPlayStationFacade) mappingSpotifyQuarterly(spotifyQuarterly dto.SpotifyQuarterlyRequestDTO) model.SpotifyQuarterly {
+	return model.SpotifyQuarterly{
+		Date:                       spotifyQuarterly.Date,
+		TotalRevenue:               spotifyQuarterly.TotalRevenue,
+		CostOfRevenue:              spotifyQuarterly.CostOfRevenue,
+		GrossProfit:                spotifyQuarterly.GrossProfit,
+		PremiumRevenue:             spotifyQuarterly.PremiumRevenue,
+		PremiumCostRevenue:         spotifyQuarterly.PremiumCostRevenue,
+		PremiumGrossProfit:         spotifyQuarterly.PremiumGrossProfit,
+		AdRevenue:                  spotifyQuarterly.AdRevenue,
+		AdCostOfRevenue:            spotifyQuarterly.AdCostOfRevenue,
+		AdGrossProfit:              spotifyQuarterly.AdGrossProfit,
+		MAUs:                       spotifyQuarterly.MAUs,
+		PremiumARPU:                spotifyQuarterly.PremiumARPU,
+		AdMAUs:                     spotifyQuarterly.AdMAUs,
+		SalesAndMarketingCost:      spotifyQuarterly.SalesAndMarketingCost,
+		ResearchAndDevelopmentCost: spotifyQuarterly.ResearchAndDevelopmentCost,
+		GenrealAndAdminstraiveCost: spotifyQuarterly.GenrealAndAdminstraiveCost,
+	}
 }
