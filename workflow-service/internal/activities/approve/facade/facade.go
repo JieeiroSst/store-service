bdca37c23dff200adc55b9856f975bfa4f086946
manh quan <luumanhquan.91@gmail.com>
2023-06-25 16:39:16 +0700

    update code

--- workflow-service/internal/activities/approve/facade/facade.go
+++ workflow-service/internal/activities/approve/facade/facade.go
@@ -1,6 +1,8 @@
 package facade
 
 import (
+	"fmt"
+
 	"github.com/JIeeiroSst/workflow-service/internal/repository"
 	"go.temporal.io/sdk/client"
 )
@@ -17,15 +19,22 @@ type Facade struct {
 	SellingPlayStation
 }
 
-func NewFacde(deps Dependency) *Facade {
-	seattleWeatherFacade := NewSeattleWeatherFacade(deps.Repository)
-	gameFace := NewGameFacade(deps.Repository)
-	spotifyQuarterly := NewSpotifyQuarterlyFacade(deps.Repository)
-	sellingPlayStation := NewSellingPlayStationFacade(deps.Repository)
-	return &Facade{
-		SeattleWeather:     seattleWeatherFacade,
-		Game:               gameFace,
-		SpotifyQuarterly:   spotifyQuarterly,
-		SellingPlayStation: sellingPlayStation,
+func NewFacde(deps Dependency, types TYPE) (resp interface{}) {
+	switch types {
+	case SeattleWeatherType:
+		seattleWeatherFacade := NewSeattleWeatherFacade(deps.Repository)
+		resp = seattleWeatherFacade
+	case GameType:
+		gameFace := NewGameFacade(deps.Repository)
+		resp = gameFace
+	case SpotifyQuarterlyType:
+		spotifyQuarterly := NewSpotifyQuarterlyFacade(deps.Repository)
+		resp = spotifyQuarterly
+	case SellingPlayStationType:
+		sellingPlayStation := NewSellingPlayStationFacade(deps.Repository)
+		resp = sellingPlayStation
+	default:
+		fmt.Printf("%v\n", types)
 	}
+	return
 }
--- workflow-service/internal/activities/approve/facade/type.go
+++ workflow-service/internal/activities/approve/facade/type.go
@@ -1,2 +1,10 @@
 package facade
 
+type TYPE string
+
+const (
+	SeattleWeatherType     TYPE = "seattle_weather_type"
+	GameType               TYPE = "game_type"
+	SpotifyQuarterlyType   TYPE = "spotify_quarterly_type"
+	SellingPlayStationType TYPE = "selling_playstation_type"
+)
