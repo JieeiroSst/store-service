777ad2ccf43486382c77e8ef9a6610936822eb25
manh quan <luumanhquan.91@gmail.com>
2023-09-10 02:16:23 +0700

    update code

--- livestream_service/live-app/src/App.js
+++ livestream_service/live-app/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { w3cwebsocket as WebSocket } from "websocket";
+
+const client = new WebSocket("ws://localhost:8080/stream");
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    client.onopen = () => {
+      console.log("WebSocket Client Connected");
+    };
+    client.onmessage = (message) => {
+      setMessages((prevMessages) => [...prevMessages, message.data]);
+    };
+    client.onclose = () => {
+      console.log("WebSocket Client Disconnected");
+    };
+  }, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    const message = event.target.elements.message.value;
+    client.send(message);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Streaming Application</h1>
+      <form onSubmit={sendMessage}>
+        <input type="text" name="message" />
+        <button type="submit">Send</button>
+      </form>
+      <ul>
+        {messages.map((message, index) => (
+          <li key={index}>{message}</li>
+        ))}
+      </ul>
     </div>
   );
 }
