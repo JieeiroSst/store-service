40612e821bd35b0654deb15ffd2762163c61c273
manh quan <luumanhquan.91@gmail.com>
2023-09-10 02:39:19 +0700

    update code

--- livestream_service/live-app/src/App.js
+++ livestream_service/live-app/src/App.js
@@ -2,9 +2,51 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import { w3cwebsocket as WebSocket } from "websocket";
 
+import { BrowserRouter as Router, Route, Switch,Link } from 'react-router-dom';
+
 const client = new WebSocket("ws://localhost:8080/stream");
 
 function App() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/chat">Chat</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/chat">
+            <Chat />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Chat() {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
@@ -40,5 +82,31 @@ function App() {
     </div>
   );
 }
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
+
 
 export default App;
