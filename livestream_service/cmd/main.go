418d8bac41001b50cd7a8660f4a952cfade49fe9
manh quan <luumanhquan.91@gmail.com>
2023-09-10 02:06:58 +0700

    update code

--- livestream_service/cmd/main.go
+++ livestream_service/cmd/main.go
@@ -1,33 +1,36 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
 
-    "github.com/gorilla/websocket"
+	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{}
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 func main() {
-    http.HandleFunc("/stream", handleStream)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/stream", handleStream)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handleStream(w http.ResponseWriter, r *http.Request) {
-    conn, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        log.Println(err)
-        return
-    }
-    defer conn.Close()
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
 
-    for {
-        _, msg, err := conn.ReadMessage()
-        if err != nil {
-            log.Println(err)
-            return
-        }
-        log.Printf("received: %s", msg)
-    }
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Printf("received: %s", msg)
+	}
 }
