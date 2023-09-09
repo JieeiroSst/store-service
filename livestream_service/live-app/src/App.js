8401bfdd3a8d3829e56bd22b17d88fe995250f5b
manh quan <luumanhquan.91@gmail.com>
2023-09-10 03:51:18 +0700

    update code

--- livestream_service/live-app/src/App.js
+++ livestream_service/live-app/src/App.js
@@ -1,112 +1,29 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import { w3cwebsocket as WebSocket } from "websocket";
-
-import { BrowserRouter as Router, Route, Switch,Link } from 'react-router-dom';
-
-const client = new WebSocket("ws://localhost:8080/stream");
+import {Routes, Route} from "react-router-dom";
+import About from "./component/About";
+import Blog from "./component/Blog";
+import Contact from "./component/Contact";
+import Home from "./component/Home";
+import Navbar from "./component/Navbar";
+import Services from "./component/Services";
+import Team from "./component/Team";
+import Chat from "./component/Chat";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/chat">Chat</Link>
-          </li>
-        </ul>
+    <>
+    <Navbar />
 
-        <hr />
-
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/chat">
-            <Chat />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <Routes >
+      <Route path="/" element={ <Home /> } />
+      <Route path="/blog" element={ <Blog /> } />
+      <Route path="/team" element={ <Team /> } />
+      <Route path="/about" element={ <About /> } />
+      <Route path="/services" element={ <Services /> } />
+      <Route path="/contact" element={ <Contact /> } />
+      <Route path="/chat" element={ <Chat /> } />
+    </Routes>
+    </>
   );
 }
-
-function Chat() {
-  const [messages, setMessages] = useState([]);
-
-  useEffect(() => {
-    client.onopen = () => {
-      console.log("WebSocket Client Connected");
-    };
-    client.onmessage = (message) => {
-      setMessages((prevMessages) => [...prevMessages, message.data]);
-    };
-    client.onclose = () => {
-      console.log("WebSocket Client Disconnected");
-    };
-  }, []);
-
-  const sendMessage = (event) => {
-    event.preventDefault();
-    const message = event.target.elements.message.value;
-    client.send(message);
-  };
-
-  return (
-    <div className="App">
-      <h1>Streaming Application</h1>
-      <form onSubmit={sendMessage}>
-        <input type="text" name="message" />
-        <button type="submit">Send</button>
-      </form>
-      <ul>
-        {messages.map((message, index) => (
-          <li key={index}>{message}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
-    </div>
-  );
-}
-
-
 
 export default App;
