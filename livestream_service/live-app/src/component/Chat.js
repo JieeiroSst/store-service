e00e02a591cad1e35336e22b1711c7f0fad8ec76
manh quan <luumanhquan.91@gmail.com>
2023-09-10 23:47:06 +0700

    update code

--- livestream_service/live-app/src/component/Chat.js
+++ livestream_service/live-app/src/component/Chat.js
@@ -32,7 +32,21 @@ function Chat() {
       <section class="bg-white dark:bg-gray-900">
         <div class="container px-6 py-12 mx-auto">
 
-        <div class="lg:w-1/2 lg:mx-6">
+          <div class="lg:flex lg:items-center lg:-mx-6">
+            <div
+                      class="w-full px-8 py-10 mx-auto overflow-hidden bg-white rounded-lg shadow-2xl dark:bg-gray-900 lg:max-w-xl shadow-gray-300/50 dark:shadow-black/50">
+            <form class="mt-6" onSubmit={sendMessage}>            
+              <div class="flex-1">
+                <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Message</label>
+                <textarea type="text" name="message" class="block w-full px-5 py-3 mt-2 text-gray-700 w-full px-5 py-3 mt-2 text-gray-700 border border-gray-200" />
+              </div>
+              <button type="submit" class="w-full px-6 py-3 mt-6 text-sm font-medium">Send</button>
+            </form>        
+
+            </div>
+          </div>
+
+          <div class="lg:w-1/2 lg:mx-6">
           <h1 class="text-3xl font-semibold text-gray-800 capitalize dark:text-white lg:text-4xl">
                     Content message
           </h1>
@@ -44,25 +58,8 @@ function Chat() {
             ))}
           </div>
         
-        </div>                
-          <div class="lg:flex lg:items-center lg:-mx-6">
-            <div
-                      class="w-full px-8 py-10 mx-auto overflow-hidden bg-white rounded-lg shadow-2xl dark:bg-gray-900 lg:max-w-xl shadow-gray-300/50 dark:shadow-black/50">
-            <form class="mt-6" onSubmit={sendMessage}>            
-              <div class="flex-1">
-                <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Message</label>
-                <textarea type="text" name="message" class="block w-full px-5 py-3 mt-2 text-gray-700 w-full px-5 py-3 mt-2 text-gray-700 border border-gray-200" />
-              </div>
-              <button type="submit" class="w-full px-6 py-3 mt-6 text-sm font-medium">Send</button>
-            </form>
-            <ul>
-              {messages.map((message, index) => (
-                <li key={index}>{message}</li>
-              ))}
-            </ul>          
+        </div>      
 
-            </div>
-          </div>
         </div>
        </section>
     );
