a187eef250b09e3d2306ecf5f0bc60f6f81eef80
manh quan <luumanhquan.91@gmail.com>
2023-09-12 20:56:32 +0700

    update code

---
+++ livestream_service/config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+type Config struct {
+	Server   ServerConfig
+}
+
+type ServerConfig struct {
+	ServerPort string
+}
+
+type Consul struct {
+	LockIndex int
+	Key       int
+	Flags     int
+	Value     string
+}
+
+type Dir struct {
+	HostConsul    string
+	KeyConsul     string
+	ServiceConsul string
+}
+
+func ReadFileEnv(dir string) (*Dir, error) {
+	err := godotenv.Load(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	data := &Dir{
+		HostConsul:    os.Getenv("HostConsul"),
+		KeyConsul:     os.Getenv("KeyConsul"),
+		ServiceConsul: os.Getenv("ServiceConsul"),
+	}
+	return data, nil
+}
