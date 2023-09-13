7a3895a32782ba895a9b1ef57373431c9cdf3144
manh quan <luumanhquan.91@gmail.com>
2023-09-13 23:31:22 +0700

    update code

--- share-service/server.ts
+++ share-service/server.ts
@@ -1,8 +1,11 @@
-const server = Bun.serve({
-  port: 3000,
-  fetch(request) {
-    return new Response("Welcome to Bun!");
-  },
+import Bao from "baojs";
+
+const app = new Bao();
+const port = parseInt(process.env.PORT || "8080");
+
+app.get("/", (ctx) => {
+  return ctx.sendText("Hello world from Bao.js running on Railway!");
 });
 
-console.log(`Listening on localhost:${server.port}`);
+const server = app.listen({ port: port });
+console.log(`Server listening on ${server.hostname}:${port}`);
