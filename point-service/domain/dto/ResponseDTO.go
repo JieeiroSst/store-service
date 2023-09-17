37246b8d8675e9f5626c9985bf5b669b1fed4478
manh quan <luumanhquan.91@gmail.com>
2023-09-17 17:25:22 +0700

    update code

---
+++ point-service/domain/dto/ResponseDTO.go
@@ -0,0 +1,9 @@
+package dto
+
+import "github.com/JIeeiroSst/point-service/helpers"
+
+type ResponseDTO struct {
+	Success    bool                   `json:"success"`
+	Data       interface{}            `json:"data"`
+	Pagination helpers.PaginationInfo `json:"pagination"`
+}
