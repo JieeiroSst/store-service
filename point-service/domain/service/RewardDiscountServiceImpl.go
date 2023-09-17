519f6bf0d456debdcb9f8a55cf7d60701e359162
manh quan <luumanhquan.91@gmail.com>
2023-09-17 18:03:17 +0700

    update code

--- point-service/domain/service/RewardDiscountServiceImpl.go
+++ point-service/domain/service/RewardDiscountServiceImpl.go
@@ -1,1 +1,26 @@
 package service
+
+import (
+	"context"
+
+	"github.com/JIeeiroSst/point-service/domain/dto"
+	"github.com/JIeeiroSst/point-service/infrastructure/repository"
+)
+
+type RewardDiscountServiceImpl struct {
+	rewardDiscountReposiory repository.RewardDiscountRepository
+}
+
+func InitRewardDiscountServiceImpl(rewardDiscountReposiory repository.RewardDiscountRepository) *RewardDiscountServiceImpl {
+	return &RewardDiscountServiceImpl{
+		rewardDiscountReposiory: rewardDiscountReposiory,
+	}
+}
+
+func (s *RewardDiscountServiceImpl) Create(ctx context.Context, data dto.RewardDiscountDTO) error
+
+func (s *RewardDiscountServiceImpl) Update(ctx context.Context, data dto.RewardDiscountDTO) error
+
+func (s *RewardDiscountServiceImpl) GetAll(ctx context.Context, perPage, sortOrder, cursor string) (*dto.ResponseDTO, error)
+
+func (s *RewardDiscountServiceImpl) GetByID(ctx context.Context, id string) (*dto.RewardDiscountDTO, error)
--- point-service/domain/service/RewardPointServiceImpl.go
+++ point-service/domain/service/RewardPointServiceImpl.go
@@ -1,1 +1,26 @@
 package service
+
+import (
+	"context"
+
+	"github.com/JIeeiroSst/point-service/domain/dto"
+	"github.com/JIeeiroSst/point-service/infrastructure/repository"
+)
+
+type RewardPointServiceImpl struct {
+	rewardPointRepository repository.RewardPointRepository
+}
+
+func InitRewardPointServiceImpl(rewardPointRepository repository.RewardPointRepository) *RewardPointServiceImpl {
+	return &RewardPointServiceImpl{
+		rewardPointRepository: rewardPointRepository,
+	}
+}
+
+func (s *RewardPointServiceImpl) Create(ctx context.Context, data dto.RewardPointDTO) error
+
+func (s *RewardPointServiceImpl) Update(ctx context.Context, data dto.RewardPointDTO) error
+
+func (s *RewardPointServiceImpl) GetAll(ctx context.Context, perPage, sortOrder, cursor string) (*dto.ResponseDTO, error)
+
+func (s *RewardPointServiceImpl) GetByID(ctx context.Context, id string) (*dto.RewardPointDTO, error)
