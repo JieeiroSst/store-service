a7ed672012c0db632a352f1ef36049f4dc54c7bb
manh quan <luumanhquan.91@gmail.com>
2023-09-17 17:16:54 +0700

    update code

--- point-service/infrastructure/repository/RewardPointRepsitory.go
+++ point-service/infrastructure/repository/RewardPointRepsitory.go
@@ -7,8 +7,8 @@ import (
 )
 
 type RewardPointRepository interface {
-	Create(ctx context.Context, data entity.RewardDiscount) error
-	Update(ctx context.Context, data entity.RewardDiscount) error
-	GetAll(ctx context.Context, perPage int, sortOrder, cursor string) ([]entity.ResponseEntity, error)
-	GetByID(ctx context.Context, id string) (*entity.RewardDiscount, error)
+	Create(ctx context.Context, data entity.RewardPoint) error
+	Update(ctx context.Context, data entity.RewardPoint) error
+	GetAll(ctx context.Context, perPage, sortOrder, cursor string) (*entity.ResponseEntity, error)
+	GetByID(ctx context.Context, id string) (*entity.RewardPoint, error)
 }
