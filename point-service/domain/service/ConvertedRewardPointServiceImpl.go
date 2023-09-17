fa9096f56456ea5f2144c7c9c5dd4418e284bf82
manh quan <luumanhquan.91@gmail.com>
2023-09-17 17:36:36 +0700

    update code

--- point-service/domain/service/ConvertedRewardPointServiceImpl.go
+++ point-service/domain/service/ConvertedRewardPointServiceImpl.go
@@ -1,1 +1,27 @@
 package service
+
+import (
+	"context"
+
+	"github.com/JIeeiroSst/point-service/domain/dto"
+	"github.com/JIeeiroSst/point-service/infrastructure/repository"
+)
+
+type ConvertedRewardPointServiceImpl struct {
+	convertedRewardPointRepository repository.ConvertedRewardPointRepository
+}
+
+func InitConvertedRewardPointServiceImpl(convertedRewardPointRepository repository.ConvertedRewardPointRepository) *ConvertedRewardPointServiceImpl {
+
+	return &ConvertedRewardPointServiceImpl{
+		convertedRewardPointRepository: convertedRewardPointRepository,
+	}
+}
+
+func (s *ConvertedRewardPointServiceImpl) Create(ctx context.Context, data dto.ConvertedRewardPointDTO) error
+
+func (s *ConvertedRewardPointServiceImpl) Update(ctx context.Context, data dto.ConvertedRewardPointDTO) error
+
+func (s *ConvertedRewardPointServiceImpl) GetAll(ctx context.Context, perPage int, sortOrder, cursor string) (*dto.ResponseDTO, error)
+
+func (s *ConvertedRewardPointServiceImpl) GetByID(ctx context.Context, id string) (*dto.ConvertedRewardPointDTO, error)
