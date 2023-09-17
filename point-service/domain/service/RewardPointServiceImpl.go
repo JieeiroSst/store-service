2d558761e651dba938b153d813b3bd9fd0f95ae3
manh quan <luumanhquan.91@gmail.com>
2023-09-17 18:26:57 +0700

    update code

--- point-service/domain/service/RewardPointServiceImpl.go
+++ point-service/domain/service/RewardPointServiceImpl.go
@@ -17,10 +17,38 @@ func InitRewardPointServiceImpl(rewardPointRepository repository.RewardPointRepo
 	}
 }
 
-func (s *RewardPointServiceImpl) Create(ctx context.Context, data dto.RewardPointDTO) error
+func (s *RewardPointServiceImpl) Create(ctx context.Context, data dto.RewardPointDTO) error {
+	entity := data.TransformDTOtoEntity()
 
-func (s *RewardPointServiceImpl) Update(ctx context.Context, data dto.RewardPointDTO) error
+	if err := s.rewardPointRepository.Create(ctx, entity); err != nil {
+		return err
+	}
+	return nil
+}
 
-func (s *RewardPointServiceImpl) GetAll(ctx context.Context, perPage, sortOrder, cursor string) (*dto.ResponseDTO, error)
+func (s *RewardPointServiceImpl) Update(ctx context.Context, data dto.RewardPointDTO) error {
+	entity := data.TransformDTOtoEntity()
 
-func (s *RewardPointServiceImpl) GetByID(ctx context.Context, id string) (*dto.RewardPointDTO, error)
+	if err := s.rewardPointRepository.Update(ctx, entity); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *RewardPointServiceImpl) GetAll(ctx context.Context, perPage, sortOrder, cursor string) (*dto.ResponseDTO, error) {
+	var responseDTO *dto.ResponseDTO
+	resp, err := s.rewardPointRepository.GetAll(ctx, perPage, sortOrder, cursor)
+	if err != nil {
+		return nil, err
+	}
+	return responseDTO.TransformEntityToDto(*resp), nil
+}
+
+func (s *RewardPointServiceImpl) GetByID(ctx context.Context, id string) (*dto.RewardPointDTO, error) {
+	var rewardPointDTO *dto.RewardPointDTO
+	resp, err := s.rewardPointRepository.GetByID(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return rewardPointDTO.TransformEntityToDto(*resp), nil
+}
