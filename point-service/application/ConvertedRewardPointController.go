3b30ade5b6dd8c1567107f03fa673e0d01da44c9
manh quan <luumanhquan.91@gmail.com>
2023-09-18 22:49:59 +0700

    update code

--- point-service/application/ConvertedRewardPointController.go
+++ point-service/application/ConvertedRewardPointController.go
@@ -1,6 +1,7 @@
 package application
 
 import (
+	"github.com/JIeeiroSst/point-service/domain/dto"
 	"github.com/JIeeiroSst/point-service/domain/service"
 	"github.com/gin-gonic/gin"
 )
@@ -22,17 +23,46 @@ func InitConvertedRewardPointRouter(router *gin.Engine, dsn string) {
 }
 
 func (r *ConvertedRewardPointContrller) GetConvertedRewardPointHandler(c *gin.Context) {
+	perPage := c.Query("per_page")
+	sortOrder := c.Query("sort_order")
+	cursor := c.Query("cursor")
 
+	response, err := r.convertedRewardPointService.GetAll(c, perPage, sortOrder, cursor)
+	if err != nil {
+		c.JSON(500, err.Error())
+	}
+	c.JSON(200, response)
 }
 
 func (r *ConvertedRewardPointContrller) GetConvertedRewardPointByIdHandler(c *gin.Context) {
-
+	id := c.Param("id")
+	response, err := r.convertedRewardPointService.GetByID(c, id)
+	if err != nil {
+		c.JSON(500, err.Error())
+	}
+	c.JSON(200, response)
 }
 
 func (r *ConvertedRewardPointContrller) CreateConvertedRewardPointHandler(c *gin.Context) {
+	var data dto.ConvertedRewardPointDTO
+	if err := c.ShouldBindJSON(&data); err != nil {
+		c.JSON(400, err.Error())
+	}
 
+	if err := r.convertedRewardPointService.Create(c, data); err != nil {
+		c.JSON(500, err.Error())
+	}
+	c.JSON(200, "create success")
 }
 
 func (r *ConvertedRewardPointContrller) UpdateConvertedRewardPointHandler(c *gin.Context) {
+	var data dto.ConvertedRewardPointDTO
+	if err := c.ShouldBindJSON(&data); err != nil {
+		c.JSON(400, err.Error())
+	}
 
+	if err := r.convertedRewardPointService.Update(c, data); err != nil {
+		c.JSON(500, err.Error())
+	}
+	c.JSON(200, "update success")
 }
