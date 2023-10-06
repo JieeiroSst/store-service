7ecb76f138b76dac172e21627bb7400cad45cfe4
manh quan <luumanhquan.91@gmail.com>
2023-10-07 04:28:01 +0700

    update code

--- partner-service/migrations/create_table.sql
+++ partner-service/migrations/create_table.sql
@@ -1,16 +1,26 @@
 create table partnerships (
-
+    id int NOT NULL PRIMARY KEY,
+    project_id int,
+    description varchar(255),
+    started_on int,
+    expires_on int,
 );
 
 create table partnerships_partners (
-
+    id int NOT NULL PRIMARY KEY,
+    partner_id int,
+    partnership_id int,
+    content varchar(255),
+    joined_on int,
+    left_on int,
 );
 
 create table partners (
-
+    id int NOT NULL PRIMARY KEY,
+    type varchar(255),
 );
 
 create table projects (
-    ID int NOT NULL PRIMARY KEY,
-    Name varchar(255),
+    id int NOT NULL PRIMARY KEY,
+    name varchar(255),
 );
