3da2d840e921344d409bb6b055699f8afac24c69
manh quan <luumanhquan.91@gmail.com>
2023-10-07 03:33:00 +0700

    update code

---
+++ partner-service/migrations/create_table.sql
@@ -0,0 +1,16 @@
+create table partnerships (
+
+);
+
+create table partnerships_partners (
+
+);
+
+create table partners (
+
+);
+
+create table projects (
+    ID int NOT NULL PRIMARY KEY,
+    Name varchar(255),
+);
