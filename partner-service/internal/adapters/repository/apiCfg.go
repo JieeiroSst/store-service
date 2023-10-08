2f0d1c03c007f61d1cf031e380fe4d715537c7f0
manh quan <luumanhquan.91@gmail.com>
2023-10-08 19:12:58 +0700

    update code

--- partner-service/internal/adapters/repository/apiCfg.go
+++ partner-service/internal/adapters/repository/apiCfg.go
@@ -1,2 +1,20 @@
 package repository
 
+import (
+	"github.com/JIeeiroSst/partner-service/internal/config"
+	"github.com/JIeeiroSst/partner-service/internal/consul"
+)
+
+func LoadAPIConfig() (*config.Config, error) {
+	dirEnv, err := config.ReadFileEnv(".env")
+	if err != nil {
+		return nil, err
+	}
+
+	consul := consul.NewConfigConsul(dirEnv.HostConsul, dirEnv.KeyConsul, dirEnv.ServiceConsul)
+	conf, err := consul.ConnectConfigConsul()
+	if err != nil {
+		return nil, err
+	}
+	return conf, nil
+}
