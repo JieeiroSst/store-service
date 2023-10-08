5b2aa2e1dd0d7ff87d955d614812fdc35ea46c2d
manh quan <luumanhquan.91@gmail.com>
2023-10-08 19:00:34 +0700

    update code

---
+++ partner-service/internal/config/config.go
@@ -0,0 +1,59 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Server ServerConfig
+	Mysql  MysqlConfig
+	Cache  CacheConfig
+}
+
+type ServerConfig struct {
+	ServerPort string
+	GRPCServer string
+}
+
+type MysqlConfig struct {
+	MysqlHost     string
+	MysqlPort     string
+	MysqlUser     string
+	MysqlPassword string
+	MysqlDbname   string
+	MysqlSSLMode  bool
+	MysqlDriver   string
+}
+
+type Consul struct {
+	LockIndex int
+	Key       int
+	Flags     int
+	Value     string
+}
+
+type Dir struct {
+	HostConsul    string
+	KeyConsul     string
+	ServiceConsul string
+}
+
+type CacheConfig struct {
+	Host string
+}
+
+func ReadFileEnv(dir string) (*Dir, error) {
+	err := godotenv.Load(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	data := &Dir{
+		HostConsul:    os.Getenv("HostConsul"),
+		KeyConsul:     os.Getenv("KeyConsul"),
+		ServiceConsul: os.Getenv("ServiceConsul"),
+	}
+	return data, nil
+}
