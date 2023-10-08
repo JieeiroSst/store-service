de6120b3e608164721f074e91fad73d4022e3ace
manh quan <luumanhquan.91@gmail.com>
2023-10-08 19:35:39 +0700

    update code

--- partner-service/internal/core/ports/ports.go
+++ partner-service/internal/core/ports/ports.go
@@ -1,2 +1,32 @@
 package ports
 
+type PartnershipRepository interface {
+
+}
+
+type PartnershipService interface {
+
+}
+
+type PartnershipsPartnerRepository interface {
+
+}
+
+type PartnershipsPartnerService interface {
+
+}
+
+type PartnerRepository interface {
+
+}
+
+type PartnerService interface {
+
+}
+
+type ProjectRepository interface{
+	
+}
+
+type ProjectService interface {
+}
