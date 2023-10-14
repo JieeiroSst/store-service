81cf99324d1a423989b14b2d2b9af8a0b59d0968
manh quan <luumanhquan.91@gmail.com>
2023-10-15 02:10:51 +0700

    update code

--- partner-service/cmd/main.go
+++ partner-service/cmd/main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/JIeeiroSst/partner-service/internal/adapters/cache"
+	"github.com/JIeeiroSst/partner-service/internal/adapters/repository"
 	"github.com/JIeeiroSst/partner-service/internal/config"
 	"github.com/JIeeiroSst/partner-service/internal/consul"
+	"github.com/JIeeiroSst/partner-service/internal/core/services"
 	"github.com/JIeeiroSst/partner-service/internal/logger"
 	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	partnershipService         *services.PartnershipService
+	partnershipsPartnerService *services.PartnershipsPartnerService
+	partnerService             *services.PartnerService
+	projectService             *services.ProjectService
 )
 
 func main() {
@@ -20,6 +34,26 @@ func main() {
 	if err != nil {
 		logger.Log.Error(err)
 	}
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		conf.Mysql.MysqlHost, conf.Mysql.MysqlPort, conf.Mysql.MysqlUser, conf.Mysql.MysqlPassword, conf.Mysql.MysqlDbname)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	redisCache, err := cache.NewRedisCache(conf.Cache.Host, "")
+	if err != nil {
+		panic(err)
+	}
+
+	store := repository.NewDB(db, redisCache)
+
+	partnershipService = services.NewPartnershipService(store)
+	partnershipsPartnerService = services.NewPartnershipsPartnerService(store)
+	partnerService = services.NewPartnerService(store)
+	projectService = services.NewProjectService(store)
 
 	InitRoutes(conf)
 }
--- partner-service/go.mod
+++ partner-service/go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0
 	github.com/joho/godotenv v1.5.1
 	github.com/sirupsen/logrus v1.9.3
+	gorm.io/driver/postgres v1.5.3
 	gorm.io/gorm v1.25.4
 )
 
@@ -33,6 +34,9 @@ require (
 	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/hashicorp/serf v0.10.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
