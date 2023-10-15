ce800e30661b654694ae3a75b8afe63f083bba4d
manh quan <luumanhquan.91@gmail.com>
2023-10-15 21:08:34 +0700

    update code

--- partner-service/internal/core/domain/model.go
+++ partner-service/internal/core/domain/model.go
@@ -1,15 +1,15 @@
 package domain
 
 type PartnershipsPartner struct {
-	ID             string        `json:"id" db:"id"`
-	Partner_id     string        `json:"partner_id" db:"partner_id"`
-	Partnership_id string        `json:"partnership_id" db:"partnership_id"`
-	Content        string        `json:"content" db:"content"`
-	JoinedOn       int           `json:"joined_on" db:"joined_on"`
-	LeftOn         int           `json:"left_on" db:"left_on"`
-	Partners       []Partner     `json:"partners"`
-	Partnerships   []Partnership `json:"partnerships"`
-	UserID         string        `json:"user_id" db:"user_id"`
+	ID            string        `json:"id" db:"id"`
+	PartnerId     string        `json:"partner_id" db:"partner_id"`
+	PartnershipId string        `json:"partnership_id" db:"partnership_id"`
+	Content       string        `json:"content" db:"content"`
+	JoinedOn      int           `json:"joined_on" db:"joined_on"`
+	LeftOn        int           `json:"left_on" db:"left_on"`
+	Partners      []Partner     `json:"partners" gorm:"foreignKey:ID;references:PartnerId"`
+	Partnerships  []Partnership `json:"partnerships" gorm:"foreignKey:ID;references:PartnershipId"`
+	UserID        string        `json:"user_id" db:"user_id"`
 }
 type Partnership struct {
 	ID          string    `json:"id" db:"id"`
@@ -17,7 +17,7 @@ type Partnership struct {
 	Description string    `json:"description" db:"description"`
 	StartedOn   int       `json:"started_on" db:"started_on"`
 	ExpiresOn   int       `json:"expires_on" db:"expires_on"`
-	Projects    []Project `json:"projects"`
+	Projects    []Project `json:"projects" gorm:"foreignKey:ID;references:ProjectId"`
 	UserID      string    `json:"user_id" db:"user_id"`
 }
 
