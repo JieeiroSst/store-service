877bc093a84a019a095f64c14ce9c8707cf97f31
manh quan <luumanhquan.91@gmail.com>
2023-10-15 20:26:59 +0700

    update code

---
+++ partner-service/internal/adapters/repository/paginate.go
@@ -0,0 +1,21 @@
+package repository
+
+import (
+	"math"
+
+	"github.com/JIeeiroSst/partner-service/internal/core/domain"
+	"gorm.io/gorm"
+)
+
+func paginate(value interface{}, pagination domain.Pagination, db *gorm.DB) func(db *gorm.DB) *gorm.DB {
+	var totalRows int64
+	db.Model(value).Count(&totalRows)
+
+	pagination.TotalRows = totalRows
+	totalPages := int(math.Ceil(float64(totalRows) / float64(pagination.Limit)))
+	pagination.TotalPages = totalPages
+
+	return func(db *gorm.DB) *gorm.DB {
+		return db.Offset(pagination.GetOffset()).Limit(pagination.GetLimit()).Order(pagination.GetSort())
+	}
+}
