df755d641ae315e586b70cabb436042d98d19a03
chmp <chmp@chmps-MacBook-Air.local>
2023-12-03 22:29:43 +0700

    upsate code

--- share-service/server.ts
+++ share-service/server.ts
@@ -1,11 +1,30 @@
 import Bao from "baojs";
+import dotenv from "dotenv";
+import OpenAI from "openai";
+
+dotenv.config();
 
 const app = new Bao();
 const port = parseInt(process.env.PORT || "8080");
+const openai = new OpenAI({
+  apiKey: process.env.API_KEY,
+});
 
 app.get("/", (ctx) => {
   return ctx.sendText("Hello world from Bao.js running on Railway!");
 });
+
+app.post("/", async (ctx) => {
+  const content = ctx.params.content
+  const result = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: 'user', content: content }],
+    stream: true,
+  });
+
+  return ctx.sendJson({ result })
+});
+
 
 const server = app.listen({ port: port });
 console.log(`Server listening on ${server.hostname}:${port}`);
