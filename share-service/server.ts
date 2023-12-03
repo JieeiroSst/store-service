0369784e7c38a35423cffd200f3f96a1778983eb
chmp <chmp@chmps-MacBook-Air.local>
2023-12-03 22:34:31 +0700

    update code

--- share-service/server.ts
+++ share-service/server.ts
@@ -10,10 +10,6 @@ const openai = new OpenAI({
   apiKey: process.env.API_KEY,
 });
 
-app.get("/", (ctx) => {
-  return ctx.sendText("Hello world from Bao.js running on Railway!");
-});
-
 app.post("/", async (ctx) => {
   const content = ctx.params.content
   const result = await openai.chat.completions.create({
