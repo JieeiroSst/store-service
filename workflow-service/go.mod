6c35ce1dc594772a8757474f2801fc4d761cbcf9
Alain Liu <luumanhquan.91@gmail.com>
2024-01-29 19:47:25 +0700

    fix bug

--- workflow-service/go.mod
+++ workflow-service/go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/lib/pq v1.10.9
 	github.com/mitchellh/mapstructure v1.5.0
 	github.com/rs/zerolog v1.29.1
+	github.com/tealeg/xlsx v1.0.5
 	go.temporal.io/sdk v1.23.0
 	golang.org/x/oauth2 v0.6.0
 	google.golang.org/api v0.114.0
@@ -51,9 +52,9 @@ require (
 	go.opencensus.io v0.24.0 // indirect
 	go.temporal.io/api v1.21.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
-	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20230525154841-bd750badd5c6 // indirect
---
+++ workflow-service/utils/excel.go
@@ -0,0 +1,67 @@
+package utils
+
+import (
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"os"
+
+	"github.com/tealeg/xlsx"
+)
+
+func ReadFileExcel(f *multipart.FileHeader) (*xlsx.File, error) {
+	tempFile, err := ioutil.TempFile("", "excel-*.xlsx")
+	if err != nil {
+		return nil, err
+	}
+	defer os.Remove(tempFile.Name())
+
+	src, err := f.Open()
+	if err != nil {
+		return nil, err
+	}
+	defer src.Close()
+
+	_, err = io.Copy(tempFile, src)
+	if err != nil {
+		return nil, err
+	}
+	xlsxFile, err := xlsx.OpenFile(tempFile.Name())
+	if err != nil {
+		return nil, err
+	}
+
+	return xlsxFile, nil
+}
+
+func GetRowValues(sheet *xlsx.Sheet, row int) []map[string]interface{} {
+	rows := sheet.MaxRow
+	cols := sheet.MaxCol
+
+	data := make([]map[string]interface{}, 0)
+
+	for r := 0; r <= rows; r++ {
+		row := make(map[string]interface{})
+		for c := 0; c <= cols; c++ {
+			cell := sheet.Cell(r, c)
+			column := sheet.Row(r).Cells[c].String()
+
+			types := cell.Type()
+
+			switch types {
+			case xlsx.CellTypeString:
+				valueStr := cell.String()
+				row[column] = valueStr
+			case xlsx.CellTypeNumeric:
+				valueNum, _ := cell.Float()
+				row[column] = valueNum
+			case xlsx.CellTypeBool:
+				valueBool := cell.Bool()
+				row[column] = valueBool
+			}
+		}
+		data = append(data, row)
+	}
+
+	return data
+}
