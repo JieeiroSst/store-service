6440be402d75b5e3eb18b5d0189d4411c960d76d
Alain Liu <luumanhquan.91@gmail.com>
2024-01-31 20:51:15 +0700

    update code

--- bookStore_service/pkg/consul/snowflake/snowflake.go
+++ bookStore_service/pkg/consul/snowflake/snowflake.go
@@ -6,17 +6,7 @@ import (
 	"github.com/bwmarrin/snowflake"
 )
 
-type snowflakeData struct{}
-
-type SnowflakeData interface {
-	GearedID() int
-}
-
-func NewSnowflake() SnowflakeData {
-	return &snowflakeData{}
-}
-
-func (s *snowflakeData) GearedID() int {
+func GearedID() int {
 	n, err := snowflake.NewNode(1)
 	if err != nil {
 		return 0
