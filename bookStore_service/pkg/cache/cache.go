d2ffa13601933d76ed3d96f533e5eb6f3087ddb1
Alain Liu <luumanhquan.91@gmail.com>
2024-01-31 21:33:38 +0700

    update code

--- bookStore_service/pkg/cache/cache.go
+++ bookStore_service/pkg/cache/cache.go
@@ -2,6 +2,7 @@ package cache
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-redis/redis/v8"
 )
@@ -23,4 +24,19 @@ func (r *Cache) HMSet(ctx context.Context, key string, object interface{}) {
 func (r *Cache) HMGet(ctx context.Context, key string, object interface{}) error {
 	scores := r.rdb.HGetAll(ctx, key).Scan(&object)
 	return scores
+}
+
+func (r *Cache) Set(ctx context.Context, key string, object interface{}) {
+	r.rdb.Set(ctx, key, object, 0).Err()
+}
+
+func (r *Cache) Get(ctx context.Context, key string, object interface{}) {
+	err := r.rdb.Get(ctx, key).Scan(&object)
+	if err != nil {
+		if err == redis.Nil {
+			fmt.Println("Key does not exist")
+		} else {
+			fmt.Println("data does not exist")
+		}
+	}
 }
