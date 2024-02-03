9353bd4040be9bcb2330fd60001d8d8925edc22e
Alain Liu <luumanhquan.91@gmail.com>
2024-02-03 19:11:35 +0700

    update code

---
+++ purchase-service/target/debug/deps/purchase_cache-d66089c948284603.d
@@ -0,0 +1,7 @@
+/Users/chmp/Desktop/store-service/purchase-service/target/debug/deps/libpurchase_cache-d66089c948284603.rmeta: purchase_cache/src/lib.rs
+
+/Users/chmp/Desktop/store-service/purchase-service/target/debug/deps/libpurchase_cache-d66089c948284603.rlib: purchase_cache/src/lib.rs
+
+/Users/chmp/Desktop/store-service/purchase-service/target/debug/deps/purchase_cache-d66089c948284603.d: purchase_cache/src/lib.rs
+
+purchase_cache/src/lib.rs:
---
+++ purchase-service/target/debug/libpurchase_cache.d
@@ -0,0 +1,1 @@
+/Users/chmp/Desktop/store-service/purchase-service/target/debug/libpurchase_cache.rlib: /Users/chmp/Desktop/store-service/purchase-service/purchase_cache/src/lib.rs
