5476887f240a54e1be8d3a29209d13c8df9dd4f2
Alain Liu <luumanhquan.91@gmail.com>
2024-02-04 02:17:27 +0700

    update code

--- purchase-service/purchase_api/src/domain/repositorie.rs
+++ purchase-service/purchase_api/src/domain/repositorie.rs
@@ -7,16 +7,15 @@ pub trait CustomerRepository {
     fn by_id(&self, id: &str) -> Result<Customer, String>;
     fn save(&self, Customer: customer);
     fn next_identity(&self) -> String;
-    fn all(&self) -> Vec<Customer>;
+    fn all(&self, limit: u64, page: u64) -> Vec<Customer>;
 }
-
 
 #[automock]
 pub trait OrdertemRepository {
     fn by_id(&self, id: &str) -> Result<Ordertem, String>;
     fn save(&self, Ordertem: ordertem);
     fn next_identity(&self) -> String;
-    fn all(&self) -> Vec<Ordertem>;
+    fn all(&self, limit: u64, page: u64) -> Vec<Ordertem>;
 }
 
 #[automock]
@@ -24,7 +23,7 @@ pub trait OrderRepository {
     fn by_id(&self, id: &str) -> Result<Order, String>;
     fn save(&self, Order: order);
     fn next_identity(&self) -> String;
-    fn all(&self) -> Vec<Order>;
+    fn all(&self, limit: u64, page: u64) -> Vec<Order>;
 }
 
 #[automock]
@@ -32,5 +31,5 @@ pub trait PosterRepository {
     fn by_id(&self, id: &str) -> Result<Poster, String>;
     fn save(&self, Poster: poster);
     fn next_identity(&self) -> String;
-    fn all(&self) -> Vec<Poster>;
+    fn all(&self, limit: u64, page: u64) -> Vec<Poster>;
 }
