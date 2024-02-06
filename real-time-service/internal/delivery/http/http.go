a532bfa77798e0b54015528019b6bd734de9072a
Alain Liu <luumanhquan.91@gmail.com>
2024-02-06 21:59:24 +0700

    update code

--- real-time-service/internal/delivery/http/http.go
+++ real-time-service/internal/delivery/http/http.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
@@ -21,6 +22,11 @@ func NewHttpDelivery(config *config.Config) *HttpDelivery {
 }
 
 func (ww *HttpDelivery) WsCall(w http.ResponseWriter, r *http.Request) {
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
 	req, err := http.NewRequest("GET", fmt.Sprintf("http://localhost:%v/ws", ww.config.Server.ServerPort), nil)
 	if err != nil {
 		log.Fatal(err)
@@ -36,16 +42,17 @@ func (ww *HttpDelivery) WsCall(w http.ResponseWriter, r *http.Request) {
 	// create ws conn
 	u := url.URL{Scheme: "ws", Host: fmt.Sprintf("http://localhost:%v", ww.config.Server.ServerPort), Path: "/ws"}
 	u.RequestURI()
-	fmt.Printf("ws url: %s", u.String())
-	log.Printf("connecting to %s", u.String())
 
 	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
 		log.Fatalf("dial err: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
-	err = conn.WriteMessage(websocket.TextMessage, []byte("hellow websockets"))
+	err = conn.WriteMessage(websocket.TextMessage, reqBody)
 	if err != nil {
 		log.Fatalf("msg err: %v", err)
 	}
+	fmt.Fprintf(w, `{"message": "successfully"}`)
 }
