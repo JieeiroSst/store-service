8a22e999858f90874e5827c3cc6e9833fb102b1e
Alain Liu <luumanhquan.91@gmail.com>
2024-02-10 21:50:42 +0700

    update code

--- search-service/internal/delivery/http/v1/task.go
+++ search-service/internal/delivery/http/v1/task.go
@@ -1,6 +1,11 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/JIeeiroSst/search-service/internal"
+	"github.com/gin-gonic/gin"
+)
 
 func (h *Handler) initSearchTaskRoutes(api *gin.RouterGroup) {
 	group := api.Group("/task")
@@ -9,5 +14,19 @@ func (h *Handler) initSearchTaskRoutes(api *gin.RouterGroup) {
 }
 
 func (h *Handler) SearchTask(c *gin.Context) {
+	var args internal.SearchParams
+	if err := c.ShouldBindJSON(&args); err != nil {
+		Response(c, http.StatusBadRequest, Message{Message: err.Error()})
+		return
+	}
+	tasks, err := h.service.Tasks.Search(c, args)
+	if err != nil {
+		Response(c, http.StatusInternalServerError, Message{Message: err.Error()})
+		return
+	}
 
+	Response(c, http.StatusOK, Message{
+		Message: "Success",
+		Data:    tasks,
+	})
 }
