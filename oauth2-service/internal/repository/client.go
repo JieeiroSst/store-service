3941f4a978bbcdeb8460787b1f62fb0cf85b2607
Alain Liu <luumanhquan.91@gmail.com>
2024-02-13 22:12:48 +0700

    oauth2 add code service

--- oauth2-service/internal/repository/client.go
+++ oauth2-service/internal/repository/client.go
@@ -1,22 +1,42 @@
 package repository
 
 import (
+	"context"
+	"errors"
 	"sync"
 
-	"github.com/redis/go-redis/v9"
-	"gorm.io/gorm"
+	"github.com/JIeeiroSst/oauth2-service/pkg/token"
 )
 
-type ClientRepository struct {
-	sync.RWMutex
-	db     *gorm.DB
-	resdis *redis.Client
+type ClientStores interface {
+	GetByID(ctx context.Context, id string) (token.ClientInfo, error)
 }
 
-func NewClientRepository(db *gorm.DB,
-	resdis *redis.Client) *ClientRepository {
-	return &ClientRepository{
-		db:     db,
-		resdis: resdis,
+func NewClientStore() *ClientStore {
+	return &ClientStore{
+		data: make(map[string]token.ClientInfo),
 	}
+}
+
+type ClientStore struct {
+	sync.RWMutex
+	data map[string]token.ClientInfo
+}
+
+func (cs *ClientStore) GetByID(ctx context.Context, id string) (token.ClientInfo, error) {
+	cs.RLock()
+	defer cs.RUnlock()
+
+	if c, ok := cs.data[id]; ok {
+		return c, nil
+	}
+	return nil, errors.New("not found")
+}
+
+func (cs *ClientStore) Set(id string, cli token.ClientInfo) (err error) {
+	cs.Lock()
+	defer cs.Unlock()
+
+	cs.data[id] = cli
+	return
 }
