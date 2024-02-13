b230b2a99283b919981372a778e23b2d34d58c90
Alain Liu <luumanhquan.91@gmail.com>
2024-02-13 21:24:11 +0700

    oauth2 add code service

--- oauth2-service/internal/usecase/usecase.go
+++ oauth2-service/internal/usecase/usecase.go
@@ -1,2 +1,18 @@
 package usecase
 
+import (
+	"github.com/JIeeiroSst/oauth2-service/internal/repository"
+	"github.com/JIeeiroSst/oauth2-service/pkg/cache"
+)
+
+type Usecase struct {
+}
+
+type Dependency struct {
+	Repos       *repository.Repositories
+	CacheHelper cache.CacheHelper
+}
+
+func NewUsecase(deps Dependency) *Usecase {
+	return &Usecase{}
+}
