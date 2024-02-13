fd14063a10ceac200cb36234d51e16b4bfaa5722
Alain Liu <luumanhquan.91@gmail.com>
2024-02-13 21:19:05 +0700

    oauth2 add code service

--- oauth2-service/cmd/main.go
+++ oauth2-service/cmd/main.go
@@ -1,5 +1,49 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/JIeeiroSst/oauth2-service/config"
+	"github.com/JIeeiroSst/oauth2-service/pkg/consul"
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	
+	router := gin.Default()
+
+	dirEnv, err := config.ReadFileEnv(".env")
+	if err != nil {
+
+	}
+
+	consul := consul.NewConfigConsul(dirEnv.HostConsul, dirEnv.KeyConsul, dirEnv.ServiceConsul)
+	conf, err := consul.ConnectConfigConsul()
+	if err != nil {
+
+	}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%v", conf.Server.ServerPort),
+		Handler: router,
+	}
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+
+	}
+	select {
+	case <-ctx.Done():
+
+	}
+
 }
