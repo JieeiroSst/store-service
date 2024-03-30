523d9055fcbb647aa74c6a000cf94b0954da87d4
Alain Liu <luumanhquan.91@gmail.com>
2024-03-31 00:43:21 +0700

    update code

--- food-service/src/main/java/com/store/foodservice/controller/FoodController.java
+++ food-service/src/main/java/com/store/foodservice/controller/FoodController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.store.foodservice.repository.FoodRepository;
+import com.store.foodservice.usecase.FoodUsecase;
 import com.store.foodservice.model.Food;
 
 @CrossOrigin(origins = "http://localhost:8080")
@@ -26,16 +26,78 @@ import com.store.foodservice.model.Food;
 @RequestMapping("/api/food")
 public class FoodController {
     @Autowired
-    FoodRepository foodRepository;
+    FoodUsecase foodUsecase;
 
-   @SuppressWarnings("null")
-   @PostMapping("")
-   public ResponseEntity<Food> createFood(@RequestBody Food food) {
-    try {
-        Food _food = foodRepository.save(food);
-        return new ResponseEntity<>(_food, HttpStatus.CREATED);
-    } catch (Exception e) {
-        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    @SuppressWarnings("null")
+    @PostMapping("")
+    public ResponseEntity<Food> createFood(@RequestBody Food food) {
+        try {
+            Food _food = foodUsecase.save(food);
+            return new ResponseEntity<>(_food, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
-   }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Food> updateFood(@PathVariable("id") long id, @RequestBody Food food) {
+        Optional<Food> FoodData = foodUsecase.findById(id);
+
+        if (FoodData.isPresent()) {
+            Food _food = FoodData.get();
+            _food.setName(food.getName());
+            _food.setDescription(food.getDescription());
+            _food.setCategory(food.getCategory());
+            _food.setPrice(food.getPrice());
+            return new ResponseEntity<>(foodUsecase.update(_food), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/")
+    public ResponseEntity<HttpStatus> deleteAllFoods() {
+        try {
+            foodUsecase.deleteAll();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteFood(@PathVariable("id") long id) {
+        try {
+            foodUsecase.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Food> getFoodById(@PathVariable("id") long id) {
+        Optional<Food> FoodData = foodUsecase.findById(id);
+
+        if (FoodData.isPresent()) {
+            return new ResponseEntity<>(FoodData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Food>> getAllFoods() {
+        try {
+            List<Food> Foods = new ArrayList<Food>();
+            foodUsecase.findAll().forEach(Foods::add);
+            if (Foods.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(Foods, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
