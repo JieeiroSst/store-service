09b2498724363b87cf92d6d5773d9e19b3827df9
Alain Liu <luumanhquan.91@gmail.com>
2024-04-06 23:30:17 +0700

    init source code c++

---
+++ node-service/README.md
@@ -0,0 +1,7 @@
+mkdir build  # Create a build directory
+
+cd build
+
+cmake ..  # Configure the project
+
+make       # Build the library
---
+++ node-service/build/CMakeFiles/node_service.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/node_service.dir/main.cpp.o"
+  "CMakeFiles/node_service.dir/main.cpp.o.d"
+  "node_service"
+  "node_service.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/node_service.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
