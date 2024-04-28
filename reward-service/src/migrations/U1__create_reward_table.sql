832832052c023f576fb7d5da67cf0ea11a0132fd
Alain Liu <luumanhquan.91@gmail.com>
2024-04-28 22:05:08 +0700

    add code

---
+++ reward-service/src/migrations/U1__create_reward_table.sql
@@ -0,0 +1,9 @@
+create table if not exists reward (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    user_id INTEGER NOT NULL,
+    points INTEGER NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_ESTAMP
+);
