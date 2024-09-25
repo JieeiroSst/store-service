9d4b1682b39e158fe4e085a172a258e15d129ecc
Alain Liu <luumanhquan.91@gmail.com>
2024-09-25 21:01:23 +0700

    add code

--- authorize_service/utils/utils.go
+++ authorize_service/utils/utils.go
@@ -4,26 +4,27 @@ import (
 	"encoding/base64"
 	"strings"
 
-	"github.com/JieeiroSst/authorize-service/pkg/log"
+	"github.com/JieeiroSst/logger"
 )
 
 func DecodeBase(msg, decode string) bool {
 	msgDecode, err := base64.StdEncoding.DecodeString(msg)
 	if err != nil {
-		log.Error(err.Error())
+		logger.ConfigZap().Error(err)
 		return false
 	}
 	if !strings.EqualFold(string(msgDecode), decode) {
-		log.Error("Decode base failed")
+		logger.ConfigZap().Error("Decode base failed")
 		return false
 	}
-	log.Info("DEcode base success")
+	logger.ConfigZap().Info("Decode base success")
 	return true
 }
 
 func DecodeByte(msg string) []byte {
 	sDec, err := base64.StdEncoding.DecodeString(msg)
 	if err != nil {
+		logger.ConfigZap().Error(err)
 		return nil
 	}
 	return sDec
