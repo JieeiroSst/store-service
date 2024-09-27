ea3cfb4f981e559c9a49380d947b01fe4f9421b2
Alain Liu <luumanhquan.91@gmail.com>
2024-09-28 00:07:28 +0700

    update code

--- restaurant/order-service/internal/delivery/consumer/consumer.go
+++ restaurant/order-service/internal/delivery/consumer/consumer.go
@@ -1,2 +1,25 @@
 package consumer
 
+import (
+	"context"
+
+	"github.com/JIeeiroSst/order-service/internal/usecase"
+)
+
+type consumer struct {
+	Usecase usecase.Usecase
+}
+
+type ConsumerInterface interface {
+	Start(ctx context.Context)
+}
+
+func NewConsumer(Usecase usecase.Usecase) ConsumerInterface {
+	return &consumer{
+		Usecase: Usecase,
+	}
+}
+
+func (c *consumer) Start(ctx context.Context) {
+
+}
