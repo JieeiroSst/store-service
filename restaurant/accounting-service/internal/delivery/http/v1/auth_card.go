aed680533bb831076041539db403ff93c3aba7af
Alain Liu <luumanhquan.91@gmail.com>
2024-10-05 00:38:38 +0700

    update code

--- restaurant/accounting-service/internal/delivery/http/v1/auth_card.go
+++ restaurant/accounting-service/internal/delivery/http/v1/auth_card.go
@@ -18,14 +18,22 @@ func (h *Handler) AuthCart(ctx *gin.Context) {
 	now := time.Now()
 	hour := now.Hour()
 
-	var order dto.Order
-	if err := ctx.ShouldBindJSON(&order); err != nil {
+	var authCart dto.AuthCart
+	if err := ctx.ShouldBindJSON(&authCart); err != nil {
 		ctx.JSON(400, gin.H{
 			"error": err,
 		})
 		return
 	}
-	orderJson, err := json.Marshal(&order)
+	orderJson, err := json.Marshal(&authCart.Order)
+	if err != nil {
+		ctx.JSON(500, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	deliveryJson, err := json.Marshal(&authCart.Delivery)
 	if err != nil {
 		ctx.JSON(500, gin.H{
 			"error": err,
@@ -34,11 +42,20 @@ func (h *Handler) AuthCart(ctx *gin.Context) {
 	}
 
 	if hour > 8 && hour < 23 {
-		if err := h.nats.Publish("order.success", orderJson); err != nil {
+		err := h.nats.Publish("order.success", orderJson)
+		if err != nil {
 			ctx.JSON(500, gin.H{
 				"error": err,
 			})
 			return
+		} else {
+			err := h.nats.Publish("delivery.ship", deliveryJson)
+			if err != nil {
+				ctx.JSON(500, gin.H{
+					"error": err,
+				})
+				return
+			}
 		}
 	} else {
 		if err := h.nats.Publish("order.reject", orderJson); err != nil {
--- restaurant/accounting-service/internal/dto/order.go
+++ restaurant/accounting-service/internal/dto/order.go
@@ -6,3 +6,14 @@ type Order struct {
 	Status    string `json:"status" form:"status"`
 	KitchenID int    `json:"kitchen_id"`
 }
+
+type Delivery struct {
+	ShipID  int    `json:"ship_id" form:"ship_id"`
+	Name    string `json:"name" form:"name"`
+	Address string `json:"address" form:"address"`
+}
+
+type AuthCart struct {
+	Order    Order    `json:"order" form:"order"`
+	Delivery Delivery `json:"delivery" form:"delivery"`
+}
