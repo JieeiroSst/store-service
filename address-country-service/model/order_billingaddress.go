aeb029522b10d7a27cb8a38bde33d5d5876d5099
Alain Liu <luumanhquan.91@gmail.com>
2024-11-10 01:56:40 +0700

    update code

--- address-country-service/model/order_billingaddress.go
+++ address-country-service/model/order_billingaddress.go
@@ -1,16 +1,17 @@
 package model
 
 type OrderBillingaddress struct {
-	ID         int    `json:"id,omitempty"`
-	Title      string `json:"title,omitempty"`
-	FirstName  string `json:"first_name,omitempty"`
-	LastName   string `json:"last_name,omitempty"`
-	Line1      string `json:"line1,omitempty"`
-	Line2      string `json:"line2,omitempty"`
-	Line3      string `json:"line3,omitempty"`
-	Line4      string `json:"line4,omitempty"`
-	State      string `json:"state,omitempty"`
-	Postcode   string `json:"postcode,omitempty"`
-	SearchText string `json:"search_text,omitempty"`
-	CountryID  string `json:"country_id,omitempty"`
+	ID             int            `json:"id,omitempty"`
+	Title          string         `json:"title,omitempty"`
+	FirstName      string         `json:"first_name,omitempty"`
+	LastName       string         `json:"last_name,omitempty"`
+	Line1          string         `json:"line1,omitempty"`
+	Line2          string         `json:"line2,omitempty"`
+	Line3          string         `json:"line3,omitempty"`
+	Line4          string         `json:"line4,omitempty"`
+	State          string         `json:"state,omitempty"`
+	Postcode       string         `json:"postcode,omitempty"`
+	SearchText     string         `json:"search_text,omitempty"`
+	CountryID      string         `json:"country_id,omitempty"`
+	AddressCountry AddressCountry `json:"address_country,omitempty" gorm:"foreignKey:CountryID"`
 }
--- address-country-service/model/shipping_orderanditemchanges_country.go
+++ address-country-service/model/shipping_orderanditemchanges_country.go
@@ -1,7 +1,8 @@
 package model
 
 type ShippingOrderanditemchangesCountry struct {
-	ID                    int    `json:"id,omitempty"`
-	OrderanditemchangesID int    `json:"orderanditemchanges_id,omitempty"`
-	CountryID             string `json:"country_id,omitempty"`
+	ID                    int            `json:"id,omitempty"`
+	OrderanditemchangesID int            `json:"orderanditemchanges_id,omitempty"`
+	CountryID             string         `json:"country_id,omitempty"`
+	AddressCountry        AddressCountry `json:"address_country,omitempty" gorm:"foreignKey:CountryID"`
 }
