60ec0832a7f882881964076172cdbe32a5125b2d
Alain Liu <luumanhquan.91@gmail.com>
2024-11-10 01:57:37 +0700

    update code

--- address-country-service/dto/shipping_orderanditemchanges_country.go
+++ address-country-service/dto/shipping_orderanditemchanges_country.go
@@ -1,7 +1,8 @@
 package dto
 
 type ShippingOrderanditemchangesCountry struct {
-	ID                    int    `json:"id,omitempty"`
-	OrderanditemchangesID int    `json:"orderanditemchanges_id,omitempty"`
-	CountryID             string `json:"country_id,omitempty"`
+	ID                    int            `json:"id,omitempty"`
+	OrderanditemchangesID int            `json:"orderanditemchanges_id,omitempty"`
+	CountryID             string         `json:"country_id,omitempty"`
+	AddressCountry        AddressCountry `json:"address_country,omitempty"`
 }
