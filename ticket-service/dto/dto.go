b314f831f5b483bdab6576c8bfaf4a9dc1d600d1
Alain Liu <luumanhquan.91@gmail.com>
2024-11-24 20:59:02 +0700

    update table

--- ticket-service/dto/dto.go
+++ ticket-service/dto/dto.go
@@ -1,1 +1,39 @@
 package dto
+
+import "time"
+
+type Customers struct {
+	CustomerID   int
+	CustomerName string
+	Phone        string
+	Email        string
+	Address      string
+}
+
+type Tickets struct {
+	TicketID    int
+	TicketName  string
+	StartDate   time.Time
+	AddressRoom string
+	Amount      float64
+	Quantity    int
+	Status      string
+}
+
+type Invoices struct {
+	InvoicesID  int
+	CustomerID  int
+	BuyDate     time.Time
+	TotalAmount float64
+	Note        string
+	Customers   Customers
+}
+
+type InvoiceDetails struct {
+	InvoiceDetailID int
+	InvoicesID      int
+	TicketID        int
+	Quantity        int
+	Invoices        Invoices
+	Tickets         Tickets
+}
