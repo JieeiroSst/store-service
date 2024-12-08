e9edb8d750690ab2f6333d344c4ad5228d6fe42c
Alain Liu <luumanhquan.91@gmail.com>
2024-12-08 21:24:25 +0700

    update code

--- payer-service/internal/delivery/consumer/consumer.go
+++ payer-service/internal/delivery/consumer/consumer.go
@@ -2,8 +2,11 @@ package consumer
 
 import (
 	"context"
+	"encoding/json"
 
+	"github.com/JIeeiroSst/payer-service/dto"
 	"github.com/JIeeiroSst/payer-service/internal/usecase"
+	"github.com/JIeeiroSst/utils/logger"
 	"github.com/nats-io/nats.go"
 )
 
@@ -20,5 +23,14 @@ func NewConsumer(usecase *usecase.Usecase, nats *nats.Conn) *Consumer {
 }
 
 func (c *Consumer) Start(ctx context.Context) {
+	c.nats.Subscribe("payer.transaction", func(msg *nats.Msg) {
+		var createTransactionsRequest dto.CreateTransactionsRequest
+		if err := json.Unmarshal(msg.Data, &createTransactionsRequest); err != nil {
+			logger.Error(ctx, "error %v", err)
+		}
 
+		if err := c.usecase.Transactions.Transactions(ctx, createTransactionsRequest); err != nil {
+			logger.Error(ctx, "error %v", err)
+		}
+	})
 }
