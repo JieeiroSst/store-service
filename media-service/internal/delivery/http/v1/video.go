963acaf2e214ed6c244006d85cbac0dde810d3a5
Alain Liu <luumanhquan.91@gmail.com>
2024-12-29 20:43:22 +0700

    update code

--- media-service/internal/delivery/http/v1/video.go
+++ media-service/internal/delivery/http/v1/video.go
@@ -1,14 +1,33 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/JIeeiroSst/utils/response"
+	"github.com/gin-gonic/gin"
+)
 
 func (h *Handler) initVideo(api *gin.RouterGroup) {
+
 
 }
 
 func (h *Handler) UploadVideo(ctx *gin.Context) {
-	// video, err := ctx.FormFile("video")
-	// if err != nil {
+	video, err := ctx.FormFile("video")
+	if err != nil {
+		response.ResponseStatus(ctx, 400, response.MessageStatus{
+			Message: err.Error(),
+			Error:   true,
+		})
+	}
 
-	// }
+	streamURL, err := h.usecase.Videos.UploadVideo(ctx, video)
+	if err != nil {
+		response.ResponseStatus(ctx, 500, response.MessageStatus{
+			Message: err.Error(),
+			Error:   true,
+		})
+	}
+	response.ResponseStatus(ctx, 500, response.MessageStatus{
+		Data:  streamURL,
+		Error: false,
+	})
 }
