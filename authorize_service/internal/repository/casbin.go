b7e58ff1004c1ba8cd0f02a27fc6f798a1f865f1
Alain Liu <luumanhquan.91@gmail.com>
2025-03-19 20:40:25 +0700

    add log

--- authorize_service/internal/repository/casbin.go
+++ authorize_service/internal/repository/casbin.go
@@ -3,10 +3,11 @@ package repository
 import (
 	"context"
 
+	"github.com/JIeeiroSst/utils/logger"
 	"github.com/JieeiroSst/authorize-service/common"
 	"github.com/JieeiroSst/authorize-service/model"
-	"github.com/JieeiroSst/authorize-service/pkg/log"
 	"github.com/JieeiroSst/authorize-service/pkg/pagination"
+	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
 
@@ -32,38 +33,43 @@ func NewCasbinRepo(db *gorm.DB) *CasbinRepo {
 }
 
 func (c *CasbinRepo) CasbinRuleAll(ctx context.Context, p pagination.Pagination) (pagination.Pagination, error) {
+	lg := logger.WithContext(ctx)
 	var casbinRules []model.CasbinRule
 
 	c.db.Scopes(pagination.Paginate(casbinRules, &p, c.db)).Find(&casbinRules)
 	p.Rows = casbinRules
 
+	lg.Info("CasbinRuleAll", zap.Any("casbinRules", casbinRules))
+
 	return p, nil
 }
 
 func (c *CasbinRepo) CasbinRuleById(ctx context.Context, id int) (*model.CasbinRule, error) {
+	lg := logger.WithContext(ctx)
 	var casbinRule model.CasbinRule
 	query := c.db.Table("casbin_rule").Where("id = ?", id).Find(&casbinRule)
 	if query.Error != nil {
-		log.Error(query.Error.Error())
+		lg.Error("CasbinRuleById", zap.Error(query.Error))
 		return nil, query.Error
 	}
 
 	if query.RowsAffected == 0 {
-		log.Error(common.NotFound.Error())
+		lg.Error("CasbinRuleById", zap.Error(common.NotFound))
 		return nil, common.NotFound
 	}
-	log.Info(casbinRule)
+	lg.Info("CasbinRuleById", zap.Any("casbinRule", casbinRule))
 	return &casbinRule, nil
 }
 
 func (c *CasbinRepo) CreateCasbinRule(ctx context.Context, casbin model.CasbinRule) error {
+	lg := logger.WithContext(ctx)
 	query := c.db.Table("casbin_rule").Save(&casbin)
 	if query.RowsAffected == 0 {
-		log.Error(common.NotFound.Error())
+		lg.Error("CreateCasbinRule", zap.Error(common.NotFound))
 		return common.NotFound
 	}
 	if query.Error != nil {
-		log.Error(query.Error.Error())
+		lg.Error("CreateCasbinRule", zap.Error(query.Error))
 		return query.Error
 	}
 
@@ -71,12 +77,15 @@ func (c *CasbinRepo) CreateCasbinRule(ctx context.Context, casbin model.CasbinRu
 }
 
 func (c *CasbinRepo) DeleteCasbinRule(ctx context.Context, id int) error {
+	lg := logger.WithContext(ctx)
 	stmtString := "DELETE FROM `casbin_rule` where id = ?;"
 	query := c.db.Raw(stmtString, id)
 	if query.Error != nil {
+		lg.Error("DeleteCasbinRule", zap.Error(query.Error))
 		return query.Error
 	}
 	if query.RowsAffected == 0 {
+		lg.Error("DeleteCasbinRule", zap.Error(common.NotFound))
 		return common.NotFound
 	}
 
@@ -84,13 +93,14 @@ func (c *CasbinRepo) DeleteCasbinRule(ctx context.Context, id int) error {
 }
 
 func (c *CasbinRepo) UpdateCasbinRulePtype(ctx context.Context, id int, ptype string) error {
+	lg := logger.WithContext(ctx)
 	query := c.db.Table("casin_rule").Where("id = ?", id).Update("ptype", ptype)
 	if query.Error != nil {
-		log.Error(query.Error.Error())
+		lg.Error("UpdateCasbinRulePtype", zap.Error(query.Error))
 		return query.Error
 	}
 	if query.RowsAffected == 0 {
-		log.Error(common.NotFound.Error())
+		lg.Error("UpdateCasbinRulePtype", zap.Error(common.NotFound))
 		return common.NotFound
 	}
 
@@ -98,13 +108,14 @@ func (c *CasbinRepo) UpdateCasbinRulePtype(ctx context.Context, id int, ptype st
 }
 
 func (c *CasbinRepo) UpdateCasbinRuleName(ctx context.Context, id int, name string) error {
+	lg := logger.WithContext(ctx)
 	query := c.db.Table("casin_rule").Where("id = ?", id).Update("v0", name)
 	if query.Error != nil {
-		log.Error(query.Error.Error())
+		lg.Error("UpdateCasbinRuleName", zap.Error(query.Error))
 		return query.Error
 	}
 	if query.RowsAffected == 0 {
-		log.Error(common.NotFound.Error())
+		lg.Error("UpdateCasbinRuleName", zap.Error(common.NotFound))
 		return common.NotFound
 	}
 
@@ -112,13 +123,14 @@ func (c *CasbinRepo) UpdateCasbinRuleName(ctx context.Context, id int, name stri
 }
 
 func (c *CasbinRepo) UpdateCasbinRuleEndpoint(ctx context.Context, id int, endpoint string) error {
+	lg := logger.WithContext(ctx)
 	query := c.db.Table("casin_rule").Where("id = ?", id).Update("v1", endpoint)
 	if query.Error != nil {
-		log.Error(query.Error.Error())
+		lg.Error("UpdateCasbinRuleEndpoint", zap.Error(query.Error))
 		return query.Error
 	}
 	if query.RowsAffected == 0 {
-		log.Error(common.NotFound.Error())
+		lg.Error("UpdateCasbinRuleEndpoint", zap.Error(common.NotFound))
 		return common.NotFound
 	}
 
@@ -126,13 +138,14 @@ func (c *CasbinRepo) UpdateCasbinRuleEndpoint(ctx context.Context, id int, endpo
 }
 
 func (c *CasbinRepo) UpdateCasbinMethod(ctx context.Context, id int, method string) error {
+	lg := logger.WithContext(ctx)
 	query := c.db.Table("casin_rule").Where("id = ?", id).Update("v2", method)
 	if query.Error != nil {
-		log.Error(query.Error.Error())
+		lg.Error("UpdateCasbinMethod", zap.Error(query.Error))
 		return query.Error
 	}
 	if query.RowsAffected == 0 {
-		log.Error(common.NotFound.Error())
+		lg.Error("UpdateCasbinMethod", zap.Error(common.NotFound))
 		return common.NotFound
 	}
 
